5c8c8d7f9628667d9edec182ce3d46587d590645
David Vassallo <vassallo.davidm@gmail.com>
2021-12-27 21:29:29 -0500

    Tiene mensajes mejores (It has better messages)

--- basics/greet.py
+++ basics/greet.py
@@ -7,3 +7,5 @@ def hello(name, lang='en'):
         print(f'Hello, {name}!')  # fstring demonstration
     elif lang == 'es':
         print(f'¡Hola, {name}!')  # fstring demonstration en español
+    else:
+        raise ValueError(f'{lang} is an unrecognized language code')
