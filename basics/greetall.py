53f1ecee64f3c46f713d450f35a02f1d3d1fd70f
David Vassallo <vassallo.davidm@gmail.com>
2021-12-29 23:55:05 -0500

    removed some comments

--- basics/greetall.py
+++ basics/greetall.py
@@ -21,12 +21,9 @@ def run():
     except IndexError: 
         print('Did not pass a filename')
         return 1
-    # open file named "name"
     file = open(name)
-    # greet each name (probably some kind of loop here)
     for line in file: 
         hello(line.strip())
-    # close the file
     file.close()
         
 if __name__ == '__main__':  # If we are running this module as a script.
