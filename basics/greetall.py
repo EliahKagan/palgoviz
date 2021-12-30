6c50655cf18e4fb3b1b8eed0056a8dc7311154e4
David Vassallo <vassallo.davidm@gmail.com>
2021-12-29 23:58:23 -0500

    added instructions for exit codes in powershell

--- basics/greetall.py
+++ basics/greetall.py
@@ -28,4 +28,4 @@ def run():
     return 0 
         
 if __name__ == '__main__':  # If we are running this module as a script.
-    sys.exit(run())
+    sys.exit(run()) # for exit codes in powershell, $LASTEXITCODE
