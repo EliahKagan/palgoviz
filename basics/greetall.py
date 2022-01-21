aa1fcec93d7f7471eec5d7e0b3868a2dc287c002
David Vassallo <vassallo.davidm@gmail.com>
2022-01-20 19:00:43 -0500

    reduced some code duplication

--- basics/greetall.py
+++ basics/greetall.py
@@ -13,30 +13,39 @@ import sys
 from greet import hello, FORMATS
 
 
+def perror(command, message):
+    print(f'ERROR in {command}: {message}', file=sys.stderr)
+    
+
+def pwarn(command, message):
+    print(f'WARNING in {command}: {message}', file=sys.stderr)
+
+
 def run():
     """Run the script."""
     # Uses LBYL (look before you leap).
     # block comments, (VSCODE) control + K + C, uncomment control + K + U
     match sys.argv:
         case (command,):
-            print(f'ERROR in {command}: Did not pass a filename', file=sys.stderr)
+            perror(command, 'Did not a pass filename')
             return 1
         case (command, name):
             lang = 'en'
         case (command, name, lang):
             if lang not in FORMATS:
-                print(f'ERROR in {command}: Did not pass a valid language code', file=sys.stderr)
+                perror(command, 'Did not pass a valid language code')
                 return 1                   
         case (command, name, lang, *_):
-            print(f'WARNING in {command}: Too many arguments, see docstring for usage', file=sys.stderr)
-
+            pwarn(command, 'Too many arguments, see doctring for usage')
+            
     # Uses EAFP (easier to ask forgiveness than permission).
     try:
         with open(name, encoding='utf-8') as file: 
             for line in file:
                 hello(line.strip(), lang) 
     except OSError as error:
-        print(f'ERROR in {sys.argv[0]}: {error}', file=sys.stderr)
+        # Something went wrong opening or reading (or closing) the file.
+        perror(sys.argv[0], error)
         return 1
     return 0 
 
