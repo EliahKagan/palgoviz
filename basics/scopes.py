975b5c0ac21ca6422079c2e33ef538f7a2c2667e
David Vassallo <vassallo.davidm@gmail.com>
2022-01-22 18:10:34 -0500

    demonstrate static scoping

--- basics/scopes.py
+++ basics/scopes.py
@@ -2,9 +2,11 @@
 How variable scoping works in Python.
 """
 
-
+# Even though python is dynamically typed, it is statically scoped. 
+# This is why the result of this is an exception, 
+# f() attempts to print the "local variable x," which doesn't exist 
 def f():
-    if True:
+    if False:
         x = 'inner'
     print(x)
 
