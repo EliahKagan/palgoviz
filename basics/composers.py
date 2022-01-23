fa125331bf306a7f64aea80267abf1b44cc0cb40
David Vassallo <vassallo.davidm@gmail.com>
2022-01-23 16:50:41 -0500

    started function composition (compose2 implementation)

---
+++ basics/composers.py
@@ -0,0 +1,34 @@
+"""Function composition."""
+
+
+def compose2(f, g):
+    """
+    Take two unary functions and returns their composition.
+
+    >>> def square(x):
+    ...     return x**2
+    >>> fourth = compose2(square,square)
+    >>> fourth(2)
+    16
+    >>> fourth(3)
+    81
+    >>> def inc(x):
+    ...     return x + 1
+    >>> squareplusone = compose2(inc,square)
+    >>> squareplusone(2)
+    5
+    >>> squareplusone(10)
+    101
+    >>> compose2(square,inc)(2)
+    9
+    """
+    def h(x):
+        return f(g(x))
+    return h
+
+
+# Can also run:  python -m doctest composers.py
+# (Pass -v after doctest for verbose output.)
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
