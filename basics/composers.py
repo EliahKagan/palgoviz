535328cd0ca81aa4f8e993dfed0091dfa38f5732
David Vassallo <vassallo.davidm@gmail.com>
2022-01-23 17:44:27 -0500

    started work on repeat_compose

--- basics/composers.py
+++ basics/composers.py
@@ -25,6 +25,24 @@ def compose2(f, g):
     return lambda x: f(g(x))
 
 
+def repeat_compose(function, count):
+    """
+    Composes the unary function, function, with itself count times.
+    >>> def square(x):
+    ...     return x**2
+    >>> repeat_compose(square, 0)(2)
+    2
+    >>> repeat_compose(square, 1)(2)
+    4
+    >>> repeat_compose(square, 2)(2)
+    16
+    """
+    rvalue = lambda x: x
+    for i in range(count):
+        rvalue = lambda y: function(function(y))
+    return rvalue
+
+
 # Can also run:  python -m doctest composers.py
 # (Pass -v after doctest for verbose output.)
 if __name__ == '__main__':
