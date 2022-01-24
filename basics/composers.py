18cc9a932c173c2c3d3b526d97dbe6e3d0eebf43
David Vassallo <vassallo.davidm@gmail.com>
2022-01-24 16:13:08 -0500

    added a second repeat_compose that doesn't chain

--- basics/composers.py
+++ basics/composers.py
@@ -25,6 +25,29 @@ def compose2(f, g):
     return lambda x: f(g(x))
 
 
+def repeat_compose_chained(function, count):
+    """
+    Composes the unary function, function, with itself count times.
+    >>> def square(x):
+    ...     return x**2
+    >>> repeat_compose_chained(square, 0)(2)
+    2
+    >>> repeat_compose_chained(square, 1)(2)
+    4
+    >>> repeat_compose_chained(square, 2)(2)
+    16
+    >>> def inc(x): return x + 1
+    >>> repeat_compose_chained(inc, 5000)(1)
+    Traceback (most recent call last):
+      ...
+    RecursionError: maximum recursion depth exceeded
+    """
+    rvalue = lambda x: x
+    for i in range(count):
+        rvalue = compose2(function, rvalue)
+    return rvalue
+
+
 def repeat_compose(function, count):
     """
     Composes the unary function, function, with itself count times.
@@ -36,11 +59,20 @@ def repeat_compose(function, count):
     4
     >>> repeat_compose(square, 2)(2)
     16
+    >>> def inc(x): return x + 1
+    >>> repeat_compose(inc, 5000)(1)
+    5001
     """
-    rvalue = lambda x: x
-    for i in range(count):
-        rvalue = compose2(function, rvalue)
+    def rvalue(x):
+        for i in range(count):
+            x = function(x)
+        return x
     return rvalue
+
+    # rvalue = lambda x: x
+    # for i in range(count):
+    #     rvalue = compose2(function, rvalue)
+    # return rvalue    
 
 
 # Can also run:  python -m doctest composers.py
