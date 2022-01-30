29bbcf21e02ae34f3328d854d91d0fa2915c3664
David Vassallo <vassallo.davidm@gmail.com>
2022-01-29 21:11:18 -0500

    added some doctests

--- basics/recursion.py
+++ basics/recursion.py
@@ -269,6 +269,16 @@ def merge_two(values1, values2):
     
     >>> merge_two([1, 3, 5], [2, 4, 6])
     [1, 2, 3, 4, 5, 6]
+    >>> merge_two([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge_two((), [2, 4, 6])
+    [2, 4, 6]
+    >>> merge_two((), [])
+    []
+    >>> merge_two([], ())
+    []
+    >>> merge_two((), (1, 1, 4, 7, 8))
+    [1, 1, 4, 7, 8]
     """
     resultlist = list(values2)
     for v1 in values1: 
