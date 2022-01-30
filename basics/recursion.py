518327a3e0990985afd170cfb6afa2188e5cc231
David Vassallo <vassallo.davidm@gmail.com>
2022-01-30 15:36:03 -0500

    updated doctests on slow merge_two

--- basics/recursion.py
+++ basics/recursion.py
@@ -271,6 +271,8 @@ def merge_two(values1, values2):
     
     >>> merge_two([1, 3, 5], [2, 4, 6])
     [1, 2, 3, 4, 5, 6]
+    >>> merge_two([2, 4, 6], [1, 3, 5])
+    [1, 2, 3, 4, 5, 6]
     >>> merge_two([], [2, 4, 6])
     [2, 4, 6]
     >>> merge_two((), [2, 4, 6])
@@ -280,6 +282,8 @@ def merge_two(values1, values2):
     >>> merge_two([], ())
     []
     >>> merge_two((), (1, 1, 4, 7, 8))
+    [1, 1, 4, 7, 8]
+    >>> merge_two((1, 1, 4, 7, 8), ())
     [1, 1, 4, 7, 8]
     """
     resultlist = list(values2)
