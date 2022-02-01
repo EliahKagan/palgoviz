5f44f3c696eb541a99dd965198b51a6b0e6745f7
Eliah Kagan <degeneracypressure@gmail.com>
2022-02-01 12:23:44 -0500

    merge_sort_bottom_up, but it's not a stable sort
    This uses the technique presented in Dasgupta. It works, but since
    it will sometimes merge across levels, originally later elements
    can be (and, unless the input sequence's length is a power of two,
    sometimes will be) in the list passed as the first argument to the
    merging routine.

--- basics/recursion.py
+++ basics/recursion.py
@@ -1,6 +1,7 @@
 """Some recursion examples."""
 
 import bisect
+import collections
 
 
 def countdown(n):
@@ -186,7 +187,7 @@ def binary_search(values, x):
     """
 
     def help_binary(low, high):  # high is an inclusive endpoint.
-        if low > high: 
+        if low > high:
             return None
         halfway = (low + high) // 2
         if x > values[halfway]:
@@ -224,7 +225,7 @@ def binary_search_iterative(values, x):
     low = 0
     high = len(values) - 1
 
-    while low <= high: 
+    while low <= high:
         halfway = (low + high) // 2
         if x > values[halfway]:
             low = halfway + 1
@@ -232,7 +233,7 @@ def binary_search_iterative(values, x):
             high = halfway - 1
         else: # values[halfway] should = x, possibly add assert.
             return halfway
-    
+
     return None
 
 
@@ -263,12 +264,12 @@ def binary_search_good(values, x):
     return index if (index < len(values)) and (values[index] == x) else None
 
 
-def merge_two_slow(values1, values2): 
+def merge_two_slow(values1, values2):
     """
-    Return a sorted list that that takes two sorted sequences as input. 
+    Return a sorted list that that takes two sorted sequences as input.
 
     If values2 is empty, this is equivilant to a binary insertion sort
-    
+
     >>> merge_two_slow([1, 3, 5], [2, 4, 6])
     [1, 2, 3, 4, 5, 6]
     >>> merge_two_slow([2, 4, 6], [1, 3, 5])
@@ -287,18 +288,18 @@ def merge_two_slow(values1, values2):
     [1, 1, 4, 7, 8]
     """
     resultlist = list(values2)
-    for v1 in values1: 
+    for v1 in values1:
         bisect.insort(resultlist, v1)
-        
+
     return resultlist
 
 
-def merge_two(values1, values2): 
+def merge_two(values1, values2):
     """
-    Return a sorted list that that takes two sorted sequences as input. 
+    Return a sorted list that that takes two sorted sequences as input.
 
     If values2 is empty, this is equivilant to a binary insertion sort
-    
+
     >>> merge_two([1, 3, 5], [2, 4, 6])
     [1, 2, 3, 4, 5, 6]
     >>> merge_two([2, 4, 6], [1, 3, 5])
@@ -317,7 +318,7 @@ def merge_two(values1, values2):
     [1, 1, 4, 7, 8]
     """
     resultlist = []
-    index = 0 
+    index = 0
 
     for v1 in values1:
         while index < len(values2) and values2[index] < v1:
@@ -330,12 +331,12 @@ def merge_two(values1, values2):
     return resultlist
 
 
-def merge_two_alt(values1, values2): 
+def merge_two_alt(values1, values2):
     """
-    Return a sorted list that that takes two sorted sequences as input. 
+    Return a sorted list that that takes two sorted sequences as input.
 
     If values2 is empty, this is equivilant to a binary insertion sort
-    
+
     >>> merge_two_alt([1, 3, 5], [2, 4, 6])
     [1, 2, 3, 4, 5, 6]
     >>> merge_two_alt([2, 4, 6], [1, 3, 5])
@@ -355,16 +356,16 @@ def merge_two_alt(values1, values2):
     """
     resultlist = []
     index1 = 0
-    index2 = 0 
-   
+    index2 = 0
+
     while index1 < len(values1) and index2 < len(values2):
         if values1[index1] <= values2[index2]:
             resultlist.append(values1[index1])
             index1 += 1
         else:
             resultlist.append(values2[index2])
-            index2 += 1 
-    
+            index2 += 1
+
     resultlist.extend(values1[index1:] or values2[index2:])
 
     return resultlist
@@ -373,7 +374,7 @@ def merge_two_alt(values1, values2):
 def merge_sort(values):
     """
     Sorts using merge_two recursively.
-    
+
     >>> merge_sort([])
     []
     >>> merge_sort(())
@@ -390,6 +391,8 @@ def merge_sort(values):
     [-7225, -7129, -6307, 389, 1555, 1597, 2673, 3446, 5315, 5660]
     >>> merge_sort(['foo', 'bar', 'baz', 'quux', 'foobar', 'ham', 'spam', 'eggs'])
     ['bar', 'baz', 'eggs', 'foo', 'foobar', 'ham', 'quux', 'spam']
+    >>> merge_sort([0.0, 0, False])  # Succeeds, because it's a stable sort.
+    [0.0, 0, False]
     """
     def helper(values):
         # base case: length is less than 2, return the list
@@ -397,6 +400,44 @@ def merge_sort(values):
             return values
 
         halfway = len(values) // 2
-        return merge_two(helper(values[:halfway]), helper(values[halfway:])) 
+        return merge_two(helper(values[:halfway]), helper(values[halfway:]))
 
-    return helper(list(values)) 
+    return helper(list(values))
+
+
+def merge_sort_bottom_up(values):
+    """
+    Sorts bottom-up, using merge_two, iteratively.
+
+    >>> merge_sort_bottom_up([])
+    []
+    >>> merge_sort_bottom_up(())
+    []
+    >>> merge_sort_bottom_up((2,))
+    [2]
+    >>> merge_sort_bottom_up([10, 20])
+    [10, 20]
+    >>> merge_sort_bottom_up([20, 10])
+    [10, 20]
+    >>> merge_sort_bottom_up([3, 3])
+    [3, 3]
+    >>> merge_sort_bottom_up([5660, -6307, 5315, 389, 3446, 2673, 1555, -7225, 1597, -7129])
+    [-7225, -7129, -6307, 389, 1555, 1597, 2673, 3446, 5315, 5660]
+    >>> merge_sort_bottom_up(['foo', 'bar', 'baz', 'quux', 'foobar', 'ham', 'spam', 'eggs'])
+    ['bar', 'baz', 'eggs', 'foo', 'foobar', 'ham', 'quux', 'spam']
+    >>> merge_sort_bottom_up([7, 6, 5, 4, 3, 2, 1])
+    [1, 2, 3, 4, 5, 6, 7]
+    >>> merge_sort_bottom_up([0.0, 0, False])  # Fails. It's not a stable sort.
+    [0.0, 0, False]
+    """
+    if not values:
+        return []
+
+    queue = collections.deque([x] for x in values)
+
+    while len(queue) > 1:
+        left = queue.popleft()
+        right = queue.popleft()
+        queue.append(merge_two(left, right))
+
+    return queue[0]
