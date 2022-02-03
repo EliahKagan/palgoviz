0bdb9477e71f26ba2bdd3508dfb4dbe5fc3b8ea1
David Vassallo <vassallo.davidm@gmail.com>
2022-02-03 14:58:37 -0500

    map, filter, enumerate with start arg

--- basics/generators_and_comprehensions.py
+++ basics/generators_and_comprehensions.py
@@ -1,7 +1,7 @@
 """Generators and comprehensions."""
 
 
-def my_enumerate(iterable):
+def my_enumerate(iterable, start=0):
     """
     Pair up items in an iterable with indices. Like the built-in enumerate.
 
@@ -16,14 +16,24 @@ def my_enumerate(iterable):
     (3, 6)
     >>> list(my_enumerate(['ham', 'spam', 'eggs']))
     [(0, 'ham'), (1, 'spam'), (2, 'eggs')]
+    >>> men = my_enumerate(range(3,10000), 3)
+    >>> next(men)
+    (3, 3)
+    >>> next(men)
+    (4, 4)
+    >>> next(men)
+    (5, 5)
+    >>> next(men)
+    (6, 6)
+    >>> list(my_enumerate(['ham', 'spam', 'eggs'], 10))
+    [(10, 'ham'), (11, 'spam'), (12, 'eggs')]
     """
-    i = 0
     for x in iterable:
-        yield (i, x)
-        i += 1
+        yield (start, x)
+        start += 1
 
 
-def print_enumerated():
+def print_enumerated(*, start=0): # start is now a keyword only argument, meaning that user MUST use in the form print_enumerated(start=n)
     """
     Show the effect of my_enumerate on a sequence of 5, ..., 9 (inclusive).
 
@@ -33,8 +43,14 @@ def print_enumerated():
     index = 2, value = 7
     index = 3, value = 8
     index = 4, value = 9
+    >>> print_enumerated(start=7)
+    index = 7, value = 5
+    index = 8, value = 6
+    index = 9, value = 7
+    index = 10, value = 8
+    index = 11, value = 9
     """
-    for index, value in my_enumerate(range(5, 10)):
+    for index, value in my_enumerate(range(5, 10), start):
         print (f'{index = }, {value = }')
 
 
