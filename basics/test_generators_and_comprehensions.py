a07e272c77ae8ac8fbdf94faa7c652478d570234
Eliah Kagan <degeneracypressure@gmail.com>
2022-02-04 23:41:42 -0500

    runtime type checking for more of the tests themselves

--- basics/test_generators_and_comprehensions.py
+++ basics/test_generators_and_comprehensions.py
@@ -5,54 +5,57 @@
 import itertools
 import unittest
 
+from typeguard import typechecked
+
 from generators_and_comprehensions import zip_two
 
 
+@typechecked
 class ZipTwoTest(unittest.TestCase):
     """Tests for generators_and_comprehensions.zip_two."""
 
     __slots__ = ()
 
-    def test_empty_iterables_zip_empty(self):
+    def test_empty_iterables_zip_empty(self) -> None:
         """Zipping empty iterables yields no elements."""
         it = zip_two([], [])
         with self.assertRaises(StopIteration):
             next(it)
 
-    def test_nonempty_empty_zips_empty(self):
+    def test_nonempty_empty_zips_empty(self) -> None:
         """Zipping a nonempty iterable with an empty one yields no elements."""
         it = zip_two([10], [])
         with self.assertRaises(StopIteration):
             next(it)
 
-    def test_empty_nonempty_zips_empty(self):
+    def test_empty_nonempty_zips_empty(self) -> None:
         """Zipping an empty iterable with a nonempty one yields no elements."""
         it = zip_two([], [10])
         with self.assertRaises(StopIteration):
             next(it)
 
-    def test_shorter_longer_zips_shorter(self):
+    def test_shorter_longer_zips_shorter(self) -> None:
         """Zipping a shorter iterable with a longer one works correctly."""
         it = zip_two([10], [20, 30])
         self.assertSequenceEqual(list(it), [(10, 20)])
 
-    def test_longer_shorter_zips_shorter(self):
+    def test_longer_shorter_zips_shorter(self) -> None:
         """Zipping a longer iterable with a shorter one works correctly."""
         it = zip_two([10, 20], [30])
         self.assertSequenceEqual(list(it), [(10, 30)])
 
-    def test_equal_lengths_zip_all(self):
+    def test_equal_lengths_zip_all(self) -> None:
         """Zipping iterables with equally many of elements works correctly."""
         it = zip_two([10, 20, 30], ['foo', 'bar', 'baz'])
         self.assertSequenceEqual(list(it),
                                  [(10, 'foo'), (20, 'bar'), (30, 'baz')])
 
-    def test_finite_infinite_zips_finite(self):
+    def test_finite_infinite_zips_finite(self) -> None:
         """Zipping a finite iterable with an infinite one works correctly."""
         it = zip_two([10, 20, 30], itertools.count(1))
         self.assertSequenceEqual(list(it), [(10, 1), (20, 2), (30, 3)])
 
-    def test_infinite_finite_zips_finite(self):
+    def test_infinite_finite_zips_finite(self) -> None:
         """Zipping an infinite iterable with a finite one works correctly."""
         it = zip_two(itertools.count(1), [10, 20, 30])
         self.assertSequenceEqual(list(it), [(1, 10), (2, 20), (3, 30)])
--- basics/test_hola.py
+++ basics/test_hola.py
@@ -6,16 +6,19 @@ import io
 import sys
 import unittest
 
+from typeguard import typechecked
+
 import hola
 
 
+@typechecked
 class HolaTest(unittest.TestCase):
     """Tests for hola.run."""
 
     __slots__ = ('_stdin', '_stdout', '_old_stdin', '_old_stdout')
 
 
-    def setUp(self):
+    def setUp(self) -> None:
         """Redirect standard input and output."""
         self._stdin = io.StringIO('nombre')
         self._stdout = io.StringIO()
@@ -24,15 +27,19 @@ class HolaTest(unittest.TestCase):
         sys.stdin = self._stdin
         sys.stdout = self._stdout
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         """Undo standard input and output redirections."""
         sys.stdin = self._old_stdin
         sys.stdout = self._old_stdout
 
     # FIXME: This test is not specific enough. It should be split.
     #        If we mock the streams, we can split this into multiple tests.
-    def test_output_is_correct(self):
+    def test_output_is_correct(self) -> None:
         """The prompt is a question followed by a space and no newline."""
         hola.run()
         all_output = self._stdout.getvalue()
         self.assertEqual(all_output, '¿Como se llama usted? ¡Hola, nombre!\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
