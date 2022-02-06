13e3a9035a246a97e5ee8a4084f7e29d8aa6d85a
David Vassallo <vassallo.davidm@gmail.com>
2022-02-06 16:52:57 -0500

    fizzbuzz with 6 and 9 instead of 3 and 5

---
+++ basics/fizzbuzz69.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""FizzBuzz (as in fizzbuzz.py), but with 6 and 9 instead of 3 and 5."""
+
+
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 18 == 0:
+            print("FizzBuzz")
+        elif x % 9 == 0:
+            print("Buzz")
+        elif x % 6 == 0: 
+            print("Fizz")
+        else:
+            print(x)
+
+
+if __name__ == '__main__':  # If we are running this module as a script.
+    fizzbuzz()
