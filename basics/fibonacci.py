9a25b157e7b34ef1fe55fa28fd01e8a4aee48afd
David Vassallo <vassallo.davidm@gmail.com>
2022-02-13 13:57:06 -0500

    finished implementing islice version of fib_n

--- basics/fibonacci.py
+++ basics/fibonacci.py
@@ -218,6 +218,12 @@ def fib_n(n):
     >>> list(fib_n(True))  # OK, since bool is a subclass of int.
     [0]
     """
+    if n < 0:
+        raise ValueError(f"can't yield negatively many Fibonacci numbers")
+
+    if not isinstance(n, int):
+        raise TypeError('n must be an int')
+        
     return itertools.islice(fib(), n)
 
 
