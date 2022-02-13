4f22dbc4ffbb978cfed93ab3bf19a58750351509
David Vassallo <vassallo.davidm@gmail.com>
2022-02-13 14:51:13 -0500

    slight further refactoring

--- basics/fibonacci.py
+++ basics/fibonacci.py
@@ -61,12 +61,11 @@ def fibonacci_better(n):
     def helper(n):
         if n == 0 or n == 1:
             d[n] = n
-            return d[n]
+            return d[n]  
         if n in d:
             return d[n]
-        else:
-            d[n] = helper(n-1) + helper(n-2)
-            return d[n]
+        d[n] = helper(n-1) + helper(n-2)
+        return d[n]
     return helper(n)
 
 
