4a0c84dfc71f3287013ec6e9af43de13d26ee7ea
David Vassallo <vassallo.davidm@gmail.com>
2022-02-14 14:07:43 -0500

    doctest showing that peek_arg prints str(arg)

--- basics/decorators.py
+++ basics/decorators.py
@@ -11,10 +11,15 @@ def peek_arg(func):
     square(3)
     >>> result
     9
+    >>> def hello(name): print(f'Hello, {name}!')
+    >>> phello = peek_arg(hello)
+    >>> phello('Bob')
+    hello(Bob)
+    Hello, Bob!
     """
 
-    def wrapper(n):
-        print(f'{func.__name__}({n})')
-        return func(n)
+    def wrapper(arg):
+        print(f'{func.__name__}({arg})')
+        return func(arg)
 
     return wrapper
