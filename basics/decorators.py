dd127a48773f3e1e25089f399a1713b8dc8d0018
David Vassallo <vassallo.davidm@gmail.com>
2022-02-16 14:03:13 -0500

    clarified distintion between passthrough & trivial delegation

--- basics/decorators.py
+++ basics/decorators.py
@@ -136,10 +136,13 @@ def give_metadata_from(wrapped):
 
 
 def does_nothing(func):
-    def mywrapper():
-        func()
+    # mywrapper has the same behavior as func when called with 1 argument.
+    # But they are not the same function.
+    def mywrapper(arg):
+        return func(arg)
     return mywrapper
 
 
 def does_nothing_2(func):
+    # This returns func itself.
     return func
