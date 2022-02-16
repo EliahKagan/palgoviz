4e0f417329ca8423caa93b58f7614d8b59151585
David Vassallo <vassallo.davidm@gmail.com>
2022-02-16 15:49:09 -0500

    started redemonstrating other technique to make give_metadata_from

--- basics/decorators.py
+++ basics/decorators.py
@@ -20,15 +20,19 @@ def peek_arg(func):
     hello('Bob')
     Hello, Bob!
     """
+    def give_metadata_from_func(wrapper):
+        wrapper.__name__ = func.__name__
+        wrapper.__module__ = func.__module__
+        wrapper.__qualname__ = func.__qualname__
+        wrapper.__doc__ = func.__doc__
+        wrapper.__annotations__ = func.__annotations__
+        return wrapper
+
+    #@give_metadata_from(func)
+    @give_metadata_from_func
     def wrapper(arg):
         print(f'{func.__name__}({arg!r})')
         return func(arg)
-
-    wrapper.__name__ = func.__name__
-    wrapper.__module__ = func.__module__
-    wrapper.__qualname__ = func.__qualname__
-    wrapper.__doc__ = func.__doc__
-    wrapper.__annotations__ = func.__annotations__
 
     return wrapper
 
