8a9b6b346c6293697a6740a1f2ca622c02ef5d58
Eliah Kagan <degeneracypressure@gmail.com>
2022-02-19 10:19:39 -0500

    removed fib_nest implementation body

--- basics/fibonacci.py
+++ basics/fibonacci.py
@@ -503,21 +503,7 @@ def fib_nest(n):
     >>> r
     (0, 1)
     """
-    if n < 0:
-        raise ValueError('n must be nonnegative')
-
-    a = 0
-    b = 1
-
-    if n == 0:
-        return a
-    if n == 1:
-        return b
-
-    for _ in range(n - 1):
-        a, b = b, (a, b)
-
-    return b
+    ...  # FIXME: Implement this.
 
 
 if __name__ == '__main__':
