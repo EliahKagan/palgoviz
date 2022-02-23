0a4e78d967fcd5178a87befac33e83f1eaba50a9
David Vassallo <vassallo.davidm@gmail.com>
2022-02-23 13:49:18 -0500

    can roll back even further

--- basics/fibonacci.py
+++ basics/fibonacci.py
@@ -500,9 +500,9 @@ def fib_nest(n):
     if n < 2:
         return n
 
-    a = 1
-    b = (0, 1)
-    for _ in range(n - 2):
+    a = 0
+    b = 1
+    for _ in range(n - 1):
         a, b = b, (a, b)
     return b
 
