fb4ca36afdc8df4a48ad8e76d34bd991b888ace1
David Vassallo <vassallo.davidm@gmail.com>
2022-03-04 14:56:09 -0500

    implemented printf style

--- basics/strings.py
+++ basics/strings.py
@@ -113,7 +113,10 @@ def mul_table_simple_alt():
        9  18  27  36  45  54  63  72  81  90
       10  20  30  40  50  60  70  80  90 100
     """
-    ...  # FIXME: Implement this.
+    for x in range(1, 11):
+        for y in range(1, 11):
+            print('%4d' % (x * y), end='')
+        print()
 
 
 def mul_table(n):
