96f01b87ce2cd6ae19ab7186f607d3f872bbbd82
David Vassallo <vassallo.davidm@gmail.com>
2022-03-04 14:41:25 -0500

    added multiple ways of building the times table

--- basics/strings.py
+++ basics/strings.py
@@ -3,7 +3,7 @@
 """String formatting."""
 
 
-def mul_table_simple():
+def mul_table_simple_1():
     """
     Print a formatted multiplication table from 1 times 1 up to 10 times 10.
 
@@ -14,7 +14,7 @@ def mul_table_simple():
     For formatting, this implementation only uses f-strings (or str.format, or
     the format builtin) and the print builtin. The output is not hard-coded.
 
-    >>> mul_table_simple()
+    >>> mul_table_simple_1()
        1   2   3   4   5   6   7   8   9  10
        2   4   6   8  10  12  14  16  18  20
        3   6   9  12  15  18  21  24  27  30
@@ -29,6 +29,35 @@ def mul_table_simple():
     for x in range(1, 11):
         for y in range(1, 11):
             print(f'{x * y : 4}', end='')
+        print()
+
+
+def mul_table_simple_2():
+    """
+    Print a formatted multiplication table from 1 times 1 up to 10 times 10.
+
+    The table has no labels. Numbers are right-justified. Each column has the
+    same width as every other column: just enough that every number is preceded
+    by at least one space.
+
+    For formatting, this implementation only uses f-strings (or str.format, or
+    the format builtin) and the print builtin. The output is not hard-coded.
+
+    >>> mul_table_simple_2()
+       1   2   3   4   5   6   7   8   9  10
+       2   4   6   8  10  12  14  16  18  20
+       3   6   9  12  15  18  21  24  27  30
+       4   8  12  16  20  24  28  32  36  40
+       5  10  15  20  25  30  35  40  45  50
+       6  12  18  24  30  36  42  48  54  60
+       7  14  21  28  35  42  49  56  63  70
+       8  16  24  32  40  48  56  64  72  80
+       9  18  27  36  45  54  63  72  81  90
+      10  20  30  40  50  60  70  80  90 100
+    """
+    for x in range(1, 11):
+        for y in range(1, 11):
+            print('{0:4}'.format((x*y)), end='')
         print()
 
 
