33907fc87372885615be258a7f644859e3548ba9
David Vassallo <vassallo.davidm@gmail.com>
2022-03-10 15:57:58 -0500

    refactored to use make_greeter

--- basics/hola.py
+++ basics/hola.py
@@ -2,13 +2,14 @@
 
 """Hola saluda al user en español. (Hola greets the user in Spanish.)"""
 
-from greet import hello
+from greet import make_greeter
 
 
 def run():
     """Run as a script."""
     name = input("¿Como se llama usted? ")
-    hello(name.strip(), 'es')  # strip() drops leading and trailing whitespace.
+    greeter = make_greeter('es')
+    greeter(name.strip())  # strip() drops leading and trailing whitespace.
 
 
 if __name__ == '__main__':  # If we are running this module as a script.
