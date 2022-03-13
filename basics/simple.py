fc66220516e5d30dcd55a2da7cf8f2433429b907
Eliah Kagan <degeneracypressure@gmail.com>
2022-03-13 18:10:10 -0400

    Revert "removed is_sorted & tests (revert this to start demo/exercise)"
    This reverts commit e24dac3c8ed1744418113ee66e1c18e9f5d6a3c5.

--- basics/simple.py
+++ basics/simple.py
@@ -20,3 +20,8 @@ def answer():
     Return the int said to answer the question of life/the universe/everything.
     """
     return 42
+
+
+def is_sorted(items):
+    """Check if an iterable is sorted."""
+    return True  # FIXME: Not all iterables are sorted.
--- basics/test_simple.py
+++ basics/test_simple.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from simple import MY_NONE, Widget, answer
+from simple import MY_NONE, Widget, answer, is_sorted
 
 
 class TestMyNone(unittest.TestCase):
@@ -58,6 +58,48 @@ class TestAnswer(unittest.TestCase):
     def test_the_answer_is_an_int(self):
         answer_to_the_question = answer()
         self.assertIsInstance(answer_to_the_question, int)
+
+
+class TestIsSorted(unittest.TestCase):
+    """Tests for the is_sorted function."""
+
+    __slots__ = ()
+
+    def test_empty_list_is_sorted(self):
+        items = []
+        self.assertTrue(is_sorted(items))
+
+    def test_empty_generator_is_sorted(self):
+        items = (x for x in ())
+        self.assertTrue(is_sorted(items))
+
+    def test_one_element_list_is_sorted(self):
+        items = [76]
+        self.assertTrue(is_sorted(items))
+
+    def test_one_element_generator_is_sorted(self):
+        items = (x for x in (76,))
+        self.assertTrue(is_sorted(items))
+
+    def test_ascending_two_element_list_is_sorted(self):
+        items = ['a', 'b']
+        self.assertTrue(is_sorted(items))
+
+    def test_ascending_two_element_generator_is_sorted(self):
+        items = (ch for ch in 'ab')
+        self.assertTrue(is_sorted(items))
+
+    def test_equal_two_element_list_is_sorted(self):
+        items = ['a', 'a']
+        self.assertTrue(is_sorted(items))
+
+    def test_equal_two_element_generator_is_sorted(self):
+        items = (ch for ch in 'aa')
+        self.assertTrue(is_sorted(items))
+
+    def test_sorted_short_but_nontrivial_list_is_sorted(self):
+        items = ['bar', 'baz', 'eggs', 'foo', 'foobar', 'ham', 'quux', 'spam']
+        self.assertTrue(is_sorted(items))
 
 
 if __name__ == '__main__':
