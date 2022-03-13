2c54193067a89838136cfa7d40efd62676a28116
David Vassallo <vassallo.davidm@gmail.com>
2022-03-13 17:09:08 -0400

    extracted to dict

--- basics/greetall.py
+++ basics/greetall.py
@@ -57,7 +57,10 @@ def greet_all_try(path, greeter):
         file.close()
 
 
-def run(*, names_processor=greet_all, greeter_factory=greet.FrozenGreeter):
+DEPENDENCIES = dict(names_processor=greet_all, greeter_factory=greet.FrozenGreeter)
+
+
+def run(*, names_processor, greeter_factory):
     """Run the script."""
     # Uses LBYL (look before you leap).
     # block comments, (VSCODE) control + K + C, uncomment control + K + U
@@ -90,4 +93,4 @@ def run(*, names_processor=greet_all, greeter_factory=greet.FrozenGreeter):
 
 if __name__ == '__main__':  # If we are running this module as a script.
     # For exit codes in powershell, $LASTEXITCODE.
-    sys.exit(run())
+    sys.exit(run(**DEPENDENCIES))
