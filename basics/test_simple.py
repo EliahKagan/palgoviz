dda061e5f9a0a05e52854520d278d29cc3c63545
David Vassallo <vassallo.davidm@gmail.com>
2022-03-15 12:58:24 -0400

    extracted Arrange in TestWidget to setUp method

--- basics/test_simple.py
+++ basics/test_simple.py
@@ -16,34 +16,31 @@ class TestMyNone(unittest.TestCase):
         self.assertIsNone(MY_NONE)
 
 
-# TODO: Extract common code to a setUp method.
 class TestWidget(unittest.TestCase):
     """Tests for the Widget class."""
 
     __slots__ = ()
 
+    def setUp(self):
+        self.widget = Widget('vast', 'mauve')  # Arrange
+
     def test_size_attribute_has_size(self):
-        widget = Widget('vast', 'mauve')
-        self.assertEqual(widget.size, 'vast')
+        self.assertEqual(self.widget.size, 'vast')
 
     def test_color_attribute_has_color(self):
-        widget = Widget('vast', 'mauve')
-        self.assertEqual(widget.color, 'mauve')
+        self.assertEqual(self.widget.color, 'mauve')
 
     def test_size_can_be_changed(self):
-        widget = Widget('vast', 'mauve')
-        widget.size = 'just barely visible'
-        self.assertEqual(widget.size, 'just barely visible')
+        self.widget.size = 'just barely visible'
+        self.assertEqual(self.widget.size, 'just barely visible')
 
     def test_color_can_be_changed(self):
-        widget = Widget('vast', 'mauve')  # Arrange.
-        widget.color = 'royal purple'  # Act.
-        self.assertEqual(widget.color, 'royal purple')  # Assert.
+        self.widget.color = 'royal purple'  # Act.
+        self.assertEqual(self.widget.color, 'royal purple')  # Assert.
 
     def test_new_attributes_cannot_be_added(self):
-        widget = Widget('vast', 'mauve')
         with self.assertRaises(AttributeError):
-            widget.favorite_desert = 'Sahara'
+            self.widget.favorite_desert = 'Sahara'
 
 
 class TestAnswer(unittest.TestCase):
