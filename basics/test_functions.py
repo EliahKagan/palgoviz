7ff6ece42c0ec9ea4c7464363bc0d42386ebaf9b
Eliah Kagan <degeneracypressure@gmail.com>
2022-03-21 00:13:48 -0400

    use @parameterized_class instead of inheritance for make_counter{,_alt}

--- basics/test_functions.py
+++ basics/test_functions.py
@@ -2,44 +2,43 @@
 
 """Tests for the functions in functions.py."""
 
-import abc
 import unittest
+
+from parameterized import parameterized_class
 
 import functions
 
 
-class _TestMakeCounterBase(abc.ABC, unittest.TestCase):
-    """Shared tests for the make_counter and make_counter_alt functions."""
+@parameterized_class(('implementation_name',), [
+    ('make_counter',),
+    ('make_counter_alt',),
+])
+class TestMakeCounter(unittest.TestCase):
+    """Tests for the make_counter and make_counter_alt functions."""
 
     __slots__ = ()
 
-    @property
-    @abc.abstractmethod
-    def implementation(self):
-        """The make_counter implementation that this class tests."""
-        ...
-
     def test_starts_at_zero_with_no_start_argument(self):
-        f = self.implementation()
+        f = self._implementation()
         self.assertEqual(f(), 0)
 
     def test_starts_at_start_argument_value_if_passed(self):
-        f = self.implementation(76)
+        f = self._implementation(76)
         self.assertEqual(f(), 76)
 
     def test_interleaved_counters_count_independently(self):
-        f = self.implementation()
+        f = self._implementation()
         self.assertEqual(f(), 0)
         self.assertEqual(f(), 1)
 
-        g = self.implementation()
+        g = self._implementation()
         self.assertEqual(f(), 2)
         self.assertEqual(g(), 0)
         self.assertEqual(f(), 3)
         self.assertEqual(g(), 1)
         self.assertEqual(g(), 2)
 
-        h = self.implementation(10)
+        h = self._implementation(10)
         self.assertEqual(h(), 10)
         self.assertEqual(f(), 4)
         self.assertEqual(g(), 3)
@@ -52,28 +51,10 @@ class _TestMakeCounterBase(abc.ABC, unittest.TestCase):
         self.assertEqual(f(), 5)
         self.assertEqual(h(), 14)
 
-
-class TestMakeCounter(_TestMakeCounterBase):
-    """Tests for the make_counter function."""
-
-    __slots__ = ()
-
     @property
-    def implementation(self):
-        return functions.make_counter
-
-
-class TestMakeCounterAlt(_TestMakeCounterBase):
-    """Tests for the make_counter_alt function."""
-
-    __slots__ = ()
-
-    @property
-    def implementation(self):
-        return functions.make_counter_alt
-
-
-del _TestMakeCounterBase
+    def _implementation(self):
+        """The function being tested."""
+        return getattr(functions, self.implementation_name)
 
 
 if __name__ == '__main__':
