43fa20dbce71c5e147ed9a308708fd2d10cc51e4
Eliah Kagan <degeneracypressure@gmail.com>
2022-03-30 12:21:06 -0400

    Moved BearBowl to newly created enumerations module
    Accordingly, its tests are in a new test_enumerations module.

---
+++ basics/enumerations.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""Some enumerations."""
+
+import enum
+import functools
+
+@functools.total_ordering
+@enum.unique
+class BearBowl(enum.Enum):
+    """
+    A bowl of porridge Goldilocks tasted while trespassing in a bear kitchen.
+
+    BearBowls compare by heat: a cooler bowl is less than a warmer bowl.
+
+    Temperatures are in Kelvin.
+
+    >>> BearBowl.TOO_COLD < BearBowl.JUST_RIGHT < BearBowl.TOO_HOT
+    True
+    """
+
+    TOO_HOT = 5778
+    """Approximate temperature of the sun."""
+
+    JUST_RIGHT = 288
+    """Non-fatal temperature for a bowl of porridge."""
+
+    TOO_COLD = 95
+    """Approximate surface temperature of Titan."""
+
+    def __lt__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.value < other.value
+
+    def __repr__(self):
+        return str(self)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
--- basics/simple.py
+++ basics/simple.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
+
 """Some simple code, for unit testing."""
 
 from abc import ABC, abstractmethod
-import enum
-import functools
 import itertools
 import sys
 
@@ -216,33 +216,6 @@ def make_toggle_alt(start):
     return lambda: next(it)
 
 
-@functools.total_ordering
-@enum.unique
-class BearBowl(enum.Enum):
-    """
-    A bowl of porridge Goldilocks tasted while trespassing in a bear kitchen.
-
-    BearBowls compare by heat: a cooler bowl is less than a warmer bowl.
-
-    Temperatures are in Kelvin.
-
-    >>> BearBowl.TOO_COLD < BearBowl.JUST_RIGHT < BearBowl.TOO_HOT
-    True
-    """
-
-    TOO_HOT = 5778
-    """Approximate temperature of the sun."""
-
-    JUST_RIGHT = 288
-    """Non-fatal temperature for a bowl of porridge."""
-
-    TOO_COLD = 95
-    """Approximate surface temperature of Titan."""
-
-    def __lt__(self, other):
-        if not isinstance(other, type(self)):
-            return NotImplemented
-        return self.value < other.value
-
-    def __repr__(self):
-        return str(self)
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
