63bb8837c58712ca2306cf8788b353d6f3ccce28
David Vassallo <vassallo.davidm@gmail.com>
2022-04-02 15:35:16 -0400

    Create class OrderedEnum and extract parts of BearBowl to it

--- basics/enumerations.py
+++ basics/enumerations.py
@@ -7,8 +7,18 @@ import functools
 
 
 @functools.total_ordering
+class OrderedEnum(enum.Enum):
+    """
+    Enumeration whose instances support order comparisons on their values.
+    """
+    def __lt__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.value < other.value
+
+
 @enum.unique
-class BearBowl(enum.Enum):
+class BearBowl(OrderedEnum):
     """
     A bowl of porridge Goldilocks tasted while trespassing in a bear kitchen.
 
@@ -28,11 +38,6 @@ class BearBowl(enum.Enum):
 
     TOO_HOT = 5778
     """Approximate temperature of the sun."""
-
-    def __lt__(self, other):
-        if not isinstance(other, type(self)):
-            return NotImplemented
-        return self.value < other.value
 
     def __repr__(self):
         return str(self)
