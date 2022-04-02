61cf1348adc0fd09b6aa8a7c780c8e294bd7b4d5
David Vassallo <vassallo.davidm@gmail.com>
2022-04-02 15:41:16 -0400

    Extract repr from BearBowl to another base class

--- basics/enumerations.py
+++ basics/enumerations.py
@@ -17,8 +17,16 @@ class OrderedEnum(enum.Enum):
         return self.value < other.value
 
 
+class CodeReprEnum(enum.Enum):
+    """
+    Enumeration whose instances' repr is python code.
+    """
+    def __repr__(self):
+        return str(self)
+
+
 @enum.unique
-class BearBowl(OrderedEnum):
+class BearBowl(OrderedEnum, CodeReprEnum):
     """
     A bowl of porridge Goldilocks tasted while trespassing in a bear kitchen.
 
@@ -38,9 +46,6 @@ class BearBowl(OrderedEnum):
 
     TOO_HOT = 5778
     """Approximate temperature of the sun."""
-
-    def __repr__(self):
-        return str(self)
 
 
 if __name__ == '__main__':
