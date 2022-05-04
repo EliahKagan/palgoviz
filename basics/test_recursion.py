00287d2aa043963a4b9cc3aa832b9e1ced98acc1
David Vassallo <vassallo.davidm@gmail.com>
2022-05-04 13:36:26 -0400

    Create simple test for stable merge for merge_two_slow

---
+++ basics/test_recursion.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Tests for the types in recursion.py.
+"""
+
+import unittest
+
+from compare import OrderIndistinct
+from recursion import merge_two_slow
+
+class TestMergeTwoSlow(unittest.TestCase):
+    """Tests for the merge_two_slow function."""
+
+    def test_is_a_stable_merge(self):
+        lhs = [OrderIndistinct(1), OrderIndistinct(2), OrderIndistinct(3)]
+        rhs = [OrderIndistinct(4), OrderIndistinct(5), OrderIndistinct(6)]
+        expected = [OrderIndistinct(1), OrderIndistinct(2), OrderIndistinct(3),
+                    OrderIndistinct(4), OrderIndistinct(5), OrderIndistinct(6)]
+        result = merge_two_slow(lhs, rhs)
+        self.assertListEqual(result, expected)
+
