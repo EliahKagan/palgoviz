ed3b466c722a0875ee5c44775a7ea2f51e27995c
David Vassallo <vassallo.davidm@gmail.com>
2022-05-04 15:04:18 -0400

    Parameterize test class for two way merge functions

--- basics/test_recursion.py
+++ basics/test_recursion.py
@@ -4,57 +4,64 @@
 
 import unittest
 
+from parameterized import parameterized_class
+
 from compare import OrderIndistinct, WeakDiamond
-from recursion import merge_two_slow
+from recursion import merge_two, merge_two_alt, merge_two_slow
 
 
-class TestMergeTwoSlow(unittest.TestCase):
-    """Tests for the merge_two_slow function."""
+@parameterized_class(('name', 'function'), [
+    ('merge_two_slow', staticmethod(merge_two_slow)),
+    ('merge_two', staticmethod(merge_two)),
+    ('merge_two_alt', staticmethod(merge_two_alt)),
+])
+class TestTwoWayMergers(unittest.TestCase):
+    """Tests for the two way merge functions."""
 
     def test_left_first_interleaved_merges(self):
-        result = merge_two_slow([1, 3, 5], [2, 4, 6])
+        result = self.function([1, 3, 5], [2, 4, 6])
         self.assertListEqual(result, [1, 2, 3, 4, 5, 6])
 
     def test_left_first_and_last_interleaved_merges(self):
-        result = merge_two_slow([1, 3, 7], [2, 4, 6])
+        result = self.function([1, 3, 7], [2, 4, 6])
         self.assertListEqual(result, [1, 2, 3, 4, 6, 7])
 
     def test_right_first_interleaved_merges(self):
-        result = merge_two_slow([2, 4, 6], [1, 3, 5])
+        result = self.function([2, 4, 6], [1, 3, 5])
         self.assertListEqual(result, [1, 2, 3, 4, 5, 6])
 
     def test_right_first_and_last_interleaved_merges(self):
-        result = merge_two_slow([2, 4, 6], [1, 3, 7])
+        result = self.function([2, 4, 6], [1, 3, 7])
         self.assertListEqual(result, [1, 2, 3, 4, 6, 7])
 
     def test_empty_list_on_left_gives_right_side(self):
-        result = merge_two_slow([], [2, 4, 6])
+        result = self.function([], [2, 4, 6])
         self.assertListEqual(result, [2, 4, 6])
 
     def test_empty_tuple_on_left_gives_right_side(self):
-        result = merge_two_slow((), [2, 4, 6])
+        result = self.function((), [2, 4, 6])
         self.assertListEqual(result, [2, 4, 6])
 
     def test_empty_tuple_with_empty_list_gives_empty_list(self):
-        result = merge_two_slow((), [])
+        result = self.function((), [])
         self.assertListEqual(result, [])
 
     def test_empty_list_with_empty_tuple_gives_empty_list(self):
-        result = merge_two_slow([], ())
+        result = self.function([], ())
         self.assertListEqual(result, [])
 
     def test_empty_tuple_on_left_gives_right_side_as_list(self):
         """
         Merging an empty tuple with a tuple with duplicate leading items works.
         """
-        result = merge_two_slow((), (1, 1, 4, 7, 8))
+        result = self.function((), (1, 1, 4, 7, 8))
         self.assertListEqual(result, [1, 1, 4, 7, 8])
 
     def test_empty_tuple_on_right_gives_left_side_as_list(self):
         """
         Merging a tuple with duplicate leading items with an empty tuple works.
         """
-        result = merge_two_slow((1, 1, 4, 7, 8), ())
+        result = self.function((1, 1, 4, 7, 8), ())
         self.assertListEqual(result, [1, 1, 4, 7, 8])
 
     def test_is_a_stable_merge_when_items_do_not_compare(self):
@@ -62,13 +69,13 @@ class TestMergeTwoSlow(unittest.TestCase):
         rhs = [OrderIndistinct(4), OrderIndistinct(5), OrderIndistinct(6)]
         expected = [OrderIndistinct(1), OrderIndistinct(2), OrderIndistinct(3),
                     OrderIndistinct(4), OrderIndistinct(5), OrderIndistinct(6)]
-        result = merge_two_slow(lhs, rhs)
+        result = self.function(lhs, rhs)
         self.assertListEqual(result, expected)
 
     def test_is_a_stable_merge_when_items_compare_equal(self):
         lhs = 1
         rhs = 1.0
-        r1, r2 = merge_two_slow((lhs,), (rhs,))
+        r1, r2 = self.function((lhs,), (rhs,))
         with self.subTest(result='r1'):
             self.assertIs(r1, lhs)
         with self.subTest(result='r2'):
@@ -79,7 +86,7 @@ class TestMergeTwoSlow(unittest.TestCase):
         rhs = [WeakDiamond.WEST, WeakDiamond.WEST]
         expected = [WeakDiamond.SOUTH, WeakDiamond.EAST, WeakDiamond.WEST,
                     WeakDiamond.WEST, WeakDiamond.NORTH]
-        result = merge_two_slow(lhs, rhs)
+        result = self.function(lhs, rhs)
         self.assertListEqual(result, expected)
 
 
