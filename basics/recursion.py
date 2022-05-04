b9036dd78b4f356d166c4edc45db7826f1d32cda
David Vassallo <vassallo.davidm@gmail.com>
2022-05-04 16:04:31 -0400

    Add merge argument to merge_sort

--- basics/recursion.py
+++ basics/recursion.py
@@ -389,7 +389,7 @@ def merge_two_alt(values1, values2):
 # FIXME: Let merge_sort take a keyword-only "merge" argument specifying what
 # two-way merging function to use. If absent, use merge_two or merge_two_alt.
 # Test it with three two-way mergers defined here, and with none specified.
-def merge_sort(values):
+def merge_sort(values, merge=merge_two):
     """
     Sort using merge_two recursively.
 
@@ -420,7 +420,7 @@ def merge_sort(values):
             return values
 
         halfway = len(values) // 2
-        return merge_two(helper(values[:halfway]), helper(values[halfway:]))
+        return merge(helper(values[:halfway]), helper(values[halfway:]))
 
     return helper(list(values))
 
