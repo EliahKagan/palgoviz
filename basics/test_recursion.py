c3fd228ae60f40447b2b4a5f68eb0a61128003c2
David Vassallo <vassallo.davidm@gmail.com>
2022-05-04 13:59:38 -0400

    Add test for stable merge when compare equal

--- basics/test_recursion.py
+++ basics/test_recursion.py
@@ -11,13 +11,22 @@ from recursion import merge_two_slow
 class TestMergeTwoSlow(unittest.TestCase):
     """Tests for the merge_two_slow function."""
 
-    def test_is_a_stable_merge(self):
+    def test_is_a_stable_merge_when_items_do_not_compare(self):
         lhs = [OrderIndistinct(1), OrderIndistinct(2), OrderIndistinct(3)]
         rhs = [OrderIndistinct(4), OrderIndistinct(5), OrderIndistinct(6)]
         expected = [OrderIndistinct(1), OrderIndistinct(2), OrderIndistinct(3),
                     OrderIndistinct(4), OrderIndistinct(5), OrderIndistinct(6)]
         result = merge_two_slow(lhs, rhs)
         self.assertListEqual(result, expected)
+
+    def test_is_a_stable_merge_when_items_compare_equal(self):
+        lhs = 1
+        rhs = 1.0
+        r1, r2 = merge_two_slow((lhs,), (rhs,))
+        with self.subTest(result='r1'):
+            self.assertIs(r1, lhs)
+        with self.subTest(result='r2'):
+            self.assertIs(r2, rhs)
 
 
 if __name__ == '__main__':
