4e37df033b5bf7c923db7ab60ba2e94a2fc3bb49
David Vassallo <vassallo.davidm@gmail.com>
2022-05-04 16:28:33 -0400

    Add unit tests for merge_sort

--- basics/test_recursion.py
+++ basics/test_recursion.py
@@ -7,7 +7,7 @@ import unittest
 from parameterized import parameterized_class
 
 from compare import OrderIndistinct, WeakDiamond
-from recursion import merge_two, merge_two_alt, merge_two_slow
+from recursion import merge_sort, merge_two, merge_two_alt, merge_two_slow
 
 
 @parameterized_class(('name', 'function'), [
@@ -88,6 +88,53 @@ class TestTwoWayMergers(unittest.TestCase):
                     WeakDiamond.WEST, WeakDiamond.NORTH]
         result = self.function(lhs, rhs)
         self.assertListEqual(result, expected)
+
+
+class TestMergeSort(unittest.TestCase):
+    """Tests for merge_sort function"""
+
+    def test_empty_list_sorts(self):
+        result = merge_sort([])
+        self.assertEqual(result, [])
+
+    def test_empty_tuple_sorts(self):
+        result = merge_sort(())
+        self.assertEqual(result, [])
+
+    def test_singleton_sorts(self):
+        result = merge_sort((2,))
+        self.assertEqual(result, [2])
+
+    def test_two_element_sorted_list_is_unchanged(self):
+        result = merge_sort([10, 20])
+        self.assertEqual(result, [10, 20])
+
+    def test_two_element_unsorted_list_is_sorted(self):
+        result = merge_sort([20, 10])
+        self.assertEqual(result, [10, 20])
+
+    def test_two_element_equal_list_is_unchanged(self):
+        result = merge_sort([3, 3])
+        self.assertEqual(result, [3, 3])
+
+    def test_big_list_is_sorted(self):
+        vals = [5660, -6307, 5315, 389, 3446, 2673, 1555, -7225, 1597, -7129]
+        result = merge_sort(vals)
+        expected = [-7225, -7129, -6307, 389, 1555, 1597, 2673, 3446, 5315, 5660]
+        self.assertEqual(result, expected)
+
+    def test_big_list_of_strings_is_sorted(self):
+        vals = ['foo', 'bar', 'baz', 'quux', 'foobar', 'ham', 'spam', 'eggs']
+        result = merge_sort(vals)
+        expected = ['bar', 'baz', 'eggs', 'foo', 'foobar', 'ham', 'quux', 'spam']
+        self.assertEqual(result, expected)
+
+    def test_sort_is_stable(self):
+        vals = [0.0, 0, False]
+        r1, r2, r3 = merge_sort(vals)
+        for result, expected in ((r1, vals[0]), (r2,vals[1]), (r3, vals[2])):
+            with self.subTest(result=result):
+                self.assertIs(result, expected)
 
 
 if __name__ == '__main__':
