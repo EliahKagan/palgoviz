6435069aa203d3cdb5546ec23cc2069e1f4b8501
David Vassallo <vassallo.davidm@gmail.com>
2022-05-06 14:38:24 -0400

    Draft implementation of merge_sort_bottom_up_stable

--- basics/recursion.py
+++ basics/recursion.py
@@ -476,6 +476,49 @@ def merge_sort_bottom_up_unstable(values, *, merge=merge_two):
 # merge_sort_bottom_up_unstable (above). What has to change to make it stable?
 # (Like the other merge sort implementations in this module, this should accept
 # an optional merger keyword-only argument taking a two-way merge function.)
+def merge_sort_bottom_up_stable(values, *, merge=merge_two):
+    """
+    Sort bottom-up, using a two way merge function, iteratively. Stable.
+
+    >>> merge_sort_bottom_up_stable([])
+    []
+    >>> merge_sort_bottom_up_stable(())
+    []
+    >>> merge_sort_bottom_up_stable((2,))
+    [2]
+    >>> merge_sort_bottom_up_stable([10, 20])
+    [10, 20]
+    >>> merge_sort_bottom_up_stable([20, 10])
+    [10, 20]
+    >>> merge_sort_bottom_up_stable([3, 3])
+    [3, 3]
+    >>> a = [5660, -6307, 5315, 389, 3446, 2673, 1555, -7225, 1597, -7129]
+    >>> merge_sort_bottom_up_stable(a)
+    [-7225, -7129, -6307, 389, 1555, 1597, 2673, 3446, 5315, 5660]
+    >>> b = ['foo', 'bar', 'baz', 'quux', 'foobar', 'ham', 'spam', 'eggs']
+    >>> merge_sort_bottom_up_stable(b)
+    ['bar', 'baz', 'eggs', 'foo', 'foobar', 'ham', 'quux', 'spam']
+    >>> merge_sort_bottom_up_stable([7, 6, 5, 4, 3, 2, 1])
+    [1, 2, 3, 4, 5, 6, 7]
+    >>> merge_sort_bottom_up_stable([0.0, 0, False])
+    [0.0, 0, False]
+    """
+    # FIXME: This is unstable, fix it so that it is stable
+    if not values:
+        return []
+
+    queue = collections.deque([x] for x in values)
+
+    while len(queue) > 1:
+        left = queue.popleft()
+        right = queue.popleft()
+        if len(left) < len(right):
+            queue.append(right)
+            queue.append(left)
+        else:
+            queue.append(merge(left, right))
+
+    return queue[0]
 
 
 def make_deep_tuple(depth):
