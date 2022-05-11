c086f991eb455c22c703271370e6aa62cfa08242
David Vassallo <vassallo.davidm@gmail.com>
2022-05-11 14:11:18 -0400

    Improve using two-parameter next()

--- basics/recursion.py
+++ basics/recursion.py
@@ -423,13 +423,9 @@ def insort_left_linear(sorted_items, new_item):
     >>> b
     [0.0, 0, False]
     """
-    try:
-        index, _ = next((index, element) for index, element
-                        in enumerate(sorted_items) if not (new_item > element))
-    except StopIteration:
-        sorted_items.append(new_item)
-    else:
-        sorted_items.insert(index, new_item)
+    index = next((index for index, element in enumerate(sorted_items)
+                    if not (new_item > element)), len(sorted_items))
+    sorted_items.insert(index, new_item)
 
 
 def insort_right_linear(sorted_items, new_item):
