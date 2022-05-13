ca7a701f8824532fbe64a0e66c486dae60fb740c
David Vassallo <vassallo.davidm@gmail.com>
2022-05-13 13:43:38 -0400

    Restore lazy implementation of drop

--- basics/gencomp1.py
+++ basics/gencomp1.py
@@ -396,9 +396,12 @@ def drop(iterable, n):
     if n < 0:
         raise ValueError("can't skip negatively many items")
 
-    it = iter(iterable)
-    collections.deque(itertools.islice(it, n), maxlen=0)
-    return it
+    def generate():
+        it = iter(iterable)
+        collections.deque(itertools.islice(it, n), maxlen=0)
+        yield from it
+
+    return generate()
 
 
 def last(iterable):
