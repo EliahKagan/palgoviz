9af0b3a7aab74a6a8be313bfd29bc7a7b09ae8af
David Vassallo <vassallo.davidm@gmail.com>
2022-05-18 16:05:20 -0400

    Implement FierceBobcat class

--- basics/bobcats.py
+++ basics/bobcats.py
@@ -5,6 +5,7 @@ This code modeled regular and extra-fierce bobcats, until most of it was lost
 in a bobcat attack. Fortunately, unit tests in test_bobcats.py were not harmed.
 """
 
+from numbers import Number, Complex, Real
 
 class Bobcat:
     """
@@ -49,7 +50,7 @@ class Bobcat:
         return self._name
 
 
-class FierceBobcat:
+class FierceBobcat(Bobcat):
     """
     A bobcat so fierce its name might not be unique.
 
@@ -62,3 +63,56 @@ class FierceBobcat:
 
     Instances of this class represent such fierce bobcats.
     """
+
+    __slots__ = ('_fierceness', )
+
+    FIERCENESS_CUTOFF = 9000
+
+    def __init__(self, name, fierceness):
+        """Create a FierceBobcat with a specified name and fierceness."""
+        if not isinstance(name, str):
+            raise TypeError('Names must be strings.')
+
+        if not name:
+            raise ValueError('Names must be non-empty.')
+
+        if not isinstance(fierceness, Number):
+            raise TypeError('Fierceness must be a number.')
+
+        if not isinstance(fierceness, Real):
+            raise TypeError('Fierceness must be real.')
+
+        if not fierceness > self.FIERCENESS_CUTOFF:
+            raise ValueError(f'Fiercess must be over {self.FIERCENESS_CUTOFF}')
+
+        self._name = name
+        self._fierceness = fierceness
+
+    def __repr__(self):
+        """Represent this FierceBobcat as Python code with parameter names."""
+        return f'{type(self).__name__}(name={self.name!r}, fierceness={self.fierceness!r})'
+
+    # No need to override __str__ method
+
+    def __eq__(self, other):
+        # FIXME: may not need this
+        if type(other) is Bobcat:
+            return False
+
+        if not isinstance(other, FierceBobcat):
+            return NotImplemented
+
+        return self.name == other.name and self.fierceness == other.fierceness
+
+    def __hash__(self):
+        return hash((self.name, self.fierceness))
+
+    @property
+    def name(self):
+        """The name bobcat naming robots gave to this bobcat."""
+        return self._name
+
+    @property
+    def fierceness(self):
+        """The fierceness of this fierce bobcat."""
+        return self._fierceness
