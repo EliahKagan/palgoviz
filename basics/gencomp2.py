2e9225f05b628685f2b4ea3430ca3bb21ab423c4
David Vassallo <vassallo.davidm@gmail.com>
2022-05-22 14:41:58 -0400

    Simplify assignment of small and big

--- basics/gencomp2.py
+++ basics/gencomp2.py
@@ -333,8 +333,7 @@ def dot_product(u, v):
     >>> dot_product(v, w) == dot_product(w, v) == 0
     True
     """
-    small = u if len(u) <= len(v) else v
-    big = v if small is u else u
+    small, big = (u, v) if len(u) <= len(v) else (v, u)
 
     return sum(small_value * big.get(key, 0) for key, small_value in small.items())
 
