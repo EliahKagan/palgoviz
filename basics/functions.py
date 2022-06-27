bc1745312b69a4782c72d3c0e91a9fa3ac975c4e
Eliah Kagan <degeneracypressure@gmail.com>
2022-06-27 19:15:05 -0400

    Remove functions.py implementation bodies
    So they can be reworked as review exercises (this time without the
    aid of doctests - but the unittest tests are still available, and
    the doctests an be brought back afterwards).

--- basics/functions.py
+++ basics/functions.py
@@ -26,8 +26,7 @@ def make_counter(start=0):
 
     This implementation never assigns to a variable that already exists.
     """
-    it = itertools.count(start)
-    return lambda: next(it)
+    # FIXME: Implement this.
 
 
 def make_counter_alt(start=0):
@@ -36,13 +35,7 @@ def make_counter_alt(start=0):
 
     This implementation does not involve iterators in any way.
     """
-    def counter():
-        nonlocal start
-        current = start
-        start += 1
-        return current
-
-    return counter
+    # FIXME: Implement this.
 
 
 def make_next_fibonacci():
@@ -52,8 +45,7 @@ def make_next_fibonacci():
 
     This implementation is simple, using an existing function in the project.
     """
-    it = fib()
-    return lambda: next(it)
+    # FIXME: Implement this.
 
 
 def make_next_fibonacci_alt():
@@ -64,24 +56,14 @@ def make_next_fibonacci_alt():
     This implementation is self-contained: it does not use anything defined
     outside, not even builtins. It also does not involve iterators in any way.
     """
-    a = 0
-    b = 1
-
-    def next_fib():
-        nonlocal a, b
-        ret = a
-        a, b = b, a + b
-        return ret
-
-    return next_fib
+    # FIXME: Implement this.
 
 
 def as_func(iterable):
     """
     Given an iterable, return a function that steps through it on each call.
     """
-    it = iter(iterable)
-    return lambda: next(it)
+    # FIXME: Implement this.
 
 
 def as_iterator_limited(func, end_sentinel):
@@ -89,7 +71,7 @@ def as_iterator_limited(func, end_sentinel):
     Given a parameterless function, return an iterator that calls it until
     end_sentinel is reached.
     """
-    return iter(func, end_sentinel)
+    # FIXME: Implement this.
 
 
 def as_iterator_limited_alt(func, end_sentinel):
@@ -100,11 +82,7 @@ def as_iterator_limited_alt(func, end_sentinel):
     This is an alternative implementation of as_iterator_limited. One
     implementation uses the iter builtin; the other does not.
     """
-    while True:
-        result = func()
-        if result == end_sentinel:
-            return
-        yield result
+    # FIXME: Implement this.
 
 
 def as_iterator(func):
@@ -112,7 +90,7 @@ def as_iterator(func):
     Given a parameterless function, return an iterator that repeatedly calls
     it.
     """
-    return iter(func, object())
+    # FIXME: Implement this.
 
 
 def as_iterator_alt(func):
@@ -123,8 +101,7 @@ def as_iterator_alt(func):
     This is an alternative implementation of as_iterator. One implementation
     uses the iter builtin; the other does not.
     """
-    while True:
-        yield func()
+    # FIXME: Implement this.
 
 
 def count_tree_nodes(root):
@@ -137,10 +114,7 @@ def count_tree_nodes(root):
 
     This is a simple recursive implementation. No helper function is used.
     """
-    if not isinstance(root, tuple):
-        return 1
-
-    return sum(count_tree_nodes(element) for element in root) + 1
+    # FIXME: Implement this.
 
 
 def count_tree_nodes_alt(root):
@@ -156,18 +130,7 @@ def count_tree_nodes_alt(root):
     No other callables, besides the helper and maybe builtins, are ever used.
     count_tree_nodes_alt never calls itself, nor does the helper ever call it.
     """
-    count = 0
-
-    def count_nodes(root):
-        nonlocal count
-        count +=1
-        if not isinstance(root, tuple):
-            return
-        for element in root:
-            count_nodes(element)
-
-    count_nodes(root)
-    return count
+    # FIXME: Implement this.
 
 
 def count_tree_nodes_instrumented(root):
@@ -184,13 +147,7 @@ def count_tree_nodes_instrumented(root):
     an entirely different means, such as posting it to a network server--then
     tests would need to change, but this function's implementation would not.
     """
-    global count_tree_nodes
-    non_decorated = count_tree_nodes
-    count_tree_nodes = peek_return(count_tree_nodes)
-    try:
-        return count_tree_nodes(root)
-    finally:
-        count_tree_nodes = non_decorated
+    # FIXME: Implement this.
 
 
 if __name__ == '__main__':
