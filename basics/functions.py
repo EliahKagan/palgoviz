f58cb29402e7cf2204fd53f2137149bd4d68a995
Eliah Kagan <degeneracypressure@gmail.com>
2022-06-27 19:09:47 -0400

    Temporarily remove functions.py doctests
    This is for forthcoming exercises where the doctests are not
    available to help in implementing the functions (but the unittest
    tests in test_functions.py remain available).

--- basics/functions.py
+++ basics/functions.py
@@ -25,16 +25,6 @@ def make_counter(start=0):
     Create and return a function that returns successive integers on each call.
 
     This implementation never assigns to a variable that already exists.
-
-    >>> f = make_counter()
-    >>> [f(), f()]
-    [0, 1]
-    >>> g = make_counter()
-    >>> [f(), g(), f(), g(), g()]
-    [2, 0, 3, 1, 2]
-    >>> h = make_counter(10)
-    >>> [h(), f(), g(), h(), g(), h(), h(), g(), g(), f(), h()]
-    [10, 4, 3, 11, 4, 12, 13, 5, 6, 5, 14]
     """
     it = itertools.count(start)
     return lambda: next(it)
@@ -45,16 +35,6 @@ def make_counter_alt(start=0):
     Create and return a function that returns successive integers on each call.
 
     This implementation does not involve iterators in any way.
-
-    >>> f = make_counter_alt()
-    >>> [f(), f()]
-    [0, 1]
-    >>> g = make_counter_alt()
-    >>> [f(), g(), f(), g(), g()]
-    [2, 0, 3, 1, 2]
-    >>> h = make_counter_alt(10)
-    >>> [h(), f(), g(), h(), g(), h(), h(), g(), g(), f(), h()]
-    [10, 4, 3, 11, 4, 12, 13, 5, 6, 5, 14]
     """
     def counter():
         nonlocal start
@@ -71,15 +51,6 @@ def make_next_fibonacci():
     each call.
 
     This implementation is simple, using an existing function in the project.
-
-    >>> f = make_next_fibonacci()
-    >>> [f() for _ in range(5)]
-    [0, 1, 1, 2, 3]
-    >>> g = make_next_fibonacci()
-    >>> [x for _ in range(5) for x in (g(), f())]
-    [0, 5, 1, 8, 1, 13, 2, 21, 3, 34]
-    >>> [f(), f(), g()]
-    [55, 89, 5]
     """
     it = fib()
     return lambda: next(it)
@@ -92,15 +63,6 @@ def make_next_fibonacci_alt():
 
     This implementation is self-contained: it does not use anything defined
     outside, not even builtins. It also does not involve iterators in any way.
-
-    >>> f = make_next_fibonacci_alt()
-    >>> [f() for _ in range(5)]
-    [0, 1, 1, 2, 3]
-    >>> g = make_next_fibonacci_alt()
-    >>> [x for _ in range(5) for x in (g(), f())]
-    [0, 5, 1, 8, 1, 13, 2, 21, 3, 34]
-    >>> [f(), f(), g()]
-    [55, 89, 5]
     """
     a = 0
     b = 1
@@ -117,25 +79,6 @@ def make_next_fibonacci_alt():
 def as_func(iterable):
     """
     Given an iterable, return a function that steps through it on each call.
-
-    >>> f = as_func([10, 20, 30])
-    >>> f()
-    10
-    >>> f()
-    20
-    >>> g = as_func(x**2 for x in itertools.count(2))
-    >>> f()
-    30
-    >>> g()
-    4
-    >>> f()
-    Traceback (most recent call last):
-      ...
-    StopIteration
-    >>> g()
-    9
-    >>> g()
-    16
     """
     it = iter(iterable)
     return lambda: next(it)
@@ -145,12 +88,6 @@ def as_iterator_limited(func, end_sentinel):
     """
     Given a parameterless function, return an iterator that calls it until
     end_sentinel is reached.
-
-    >>> it = as_iterator_limited(make_counter_alt(), 2000)
-    >>> list(it) == list(range(2000))
-    True
-    >>> list(as_iterator_limited(make_next_fibonacci_alt(), 89))
-    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     """
     return iter(func, end_sentinel)
 
@@ -162,12 +99,6 @@ def as_iterator_limited_alt(func, end_sentinel):
 
     This is an alternative implementation of as_iterator_limited. One
     implementation uses the iter builtin; the other does not.
-
-    >>> it = as_iterator_limited_alt(make_counter_alt(), 2000)
-    >>> list(it) == list(range(2000))
-    True
-    >>> list(as_iterator_limited_alt(make_next_fibonacci_alt(), 89))
-    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     """
     while True:
         result = func()
@@ -180,12 +111,6 @@ def as_iterator(func):
     """
     Given a parameterless function, return an iterator that repeatedly calls
     it.
-
-    >>> it = itertools.islice(as_iterator(make_counter_alt()), 2000)
-    >>> list(it) == list(range(2000))
-    True
-    >>> list(itertools.islice(as_iterator(make_next_fibonacci_alt()), 11))
-    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     """
     return iter(func, object())
 
@@ -197,12 +122,6 @@ def as_iterator_alt(func):
 
     This is an alternative implementation of as_iterator. One implementation
     uses the iter builtin; the other does not.
-
-    >>> it = itertools.islice(as_iterator_alt(make_counter_alt()), 2000)
-    >>> list(it) == list(range(2000))
-    True
-    >>> list(itertools.islice(as_iterator_alt(make_next_fibonacci_alt()), 11))
-    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     """
     while True:
         yield func()
@@ -217,19 +136,6 @@ def count_tree_nodes(root):
     are counted multiple times. No memoization is performed.
 
     This is a simple recursive implementation. No helper function is used.
-
-    >>> count_tree_nodes('a parrot')
-    1
-    >>> count_tree_nodes(())
-    1
-    >>> a = ((2, 7, 1), (8, 6), (9, (4, 5)), ((((5, 4), 3), 2), 1))
-    >>> count_tree_nodes(a)
-    22
-    >>> count_tree_nodes([a])
-    1
-    >>> from fibonacci import fib_nest
-    >>> [count_tree_nodes(fib_nest(k)) for k in range(17)]
-    [1, 1, 3, 5, 9, 15, 25, 41, 67, 109, 177, 287, 465, 753, 1219, 1973, 3193]
     """
     if not isinstance(root, tuple):
         return 1
@@ -249,19 +155,6 @@ def count_tree_nodes_alt(root):
     function that does not return a value (really, it always returns None).
     No other callables, besides the helper and maybe builtins, are ever used.
     count_tree_nodes_alt never calls itself, nor does the helper ever call it.
-
-    >>> count_tree_nodes_alt('a parrot')
-    1
-    >>> count_tree_nodes_alt(())
-    1
-    >>> a = ((2, 7, 1), (8, 6), (9, (4, 5)), ((((5, 4), 3), 2), 1))
-    >>> count_tree_nodes_alt(a)
-    22
-    >>> count_tree_nodes_alt([a])
-    1
-    >>> from fibonacci import fib_nest
-    >>> [count_tree_nodes_alt(fib_nest(k)) for k in range(17)]
-    [1, 1, 3, 5, 9, 15, 25, 41, 67, 109, 177, 287, 465, 753, 1219, 1973, 3193]
     """
     count = 0
 
@@ -290,31 +183,6 @@ def count_tree_nodes_instrumented(root):
     change--for example by using a different output format or even giving it by
     an entirely different means, such as posting it to a network server--then
     tests would need to change, but this function's implementation would not.
-
-    >>> from recursion import make_deep_tuple
-    >>> count_tree_nodes_instrumented(make_deep_tuple(2))
-    count_tree_nodes(()) -> 1
-    count_tree_nodes(((),)) -> 2
-    count_tree_nodes((((),),)) -> 3
-    3
-    >>> count_tree_nodes(make_deep_tuple(3))
-    4
-    >>> try:
-    ...     count_tree_nodes_instrumented(make_deep_tuple(5000))
-    ... except RecursionError:
-    ...     print('Got RecursionError.')
-    ...     a = ((2, 7, 1), (8, 6), (9, (4, 5)), ((((5, 4), 3), 2), 1))
-    ...     print(count_tree_nodes(a))
-    Got RecursionError.
-    22
-    >>> from fibonacci import fib_nest
-    >>> count_tree_nodes_instrumented(fib_nest(3))
-    count_tree_nodes(1) -> 1
-    count_tree_nodes(0) -> 1
-    count_tree_nodes(1) -> 1
-    count_tree_nodes((0, 1)) -> 3
-    count_tree_nodes((1, (0, 1))) -> 5
-    5
     """
     global count_tree_nodes
     non_decorated = count_tree_nodes
