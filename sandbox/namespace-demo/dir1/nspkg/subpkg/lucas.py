dc73ad2c56118a30a5b101ccabc84faa88560a95
David Vassallo <vassallo.davidm@gmail.com>
2022-07-04 16:44:37 -0400

    Start lucas.py

--- sandbox/namespace-demo/dir1/nspkg/subpkg/lucas.py
+++ sandbox/namespace-demo/dir1/nspkg/subpkg/lucas.py
@@ -1,1 +1,17 @@
-print('Han shoots first.')
+"""
+Lucas sequences.
+
+https://en.wikipedia.org/wiki/Generalizations_of_Fibonacci_numbers#Lucas_sequences
+
+(Greedo shoots first.)
+"""
+
+
+def compute_lucas_u(p, q, n):
+    """
+    Compute element n of the (p, q) Lucas "U" sequence.
+
+    This is the naive recursive algorithm, taking exponential time.
+
+    p and q may be any integers. If n is negative, ValueError is raised.
+    """
