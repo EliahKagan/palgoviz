d7d7e381a9e5bd58d2456a0687afdfa1cb124cfc
David Vassallo <vassallo.davidm@gmail.com>
2022-07-08 14:31:22 -0400

    Expand hello docstring

--- sandbox/traditional-demo/dir2/tradpkg/subpkg/hello.py
+++ sandbox/traditional-demo/dir2/tradpkg/subpkg/hello.py
@@ -1,4 +1,9 @@
-"""Say hello"""
+"""
+Say hello.
+
+This checks that traditional packages cannot be split across separate directory
+trees (unlike namespace packages).
+"""
 
 def hello():
     print('hello')
