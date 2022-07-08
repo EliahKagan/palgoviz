761eff3aff80b1a955de7bf20f7f9ccba243a6b9
David Vassallo <vassallo.davidm@gmail.com>
2022-07-08 16:00:15 -0400

    Make __main__.py more interesting

--- sandbox/traditional-demo/dir1/tradpkg/__main__.py
+++ sandbox/traditional-demo/dir1/tradpkg/__main__.py
@@ -2,8 +2,23 @@
 Print a grid.
 
 This demonstrates running the top-level package as a script.
+
+Usage example:
+
+    python -m tradpkg 3 5
 """
+
+import pprint
+import sys
 
 from .subpkg.grids import make_grid
 
-print(make_grid(4, 4))
+match sys.argv:
+    case [_]:
+        m = 5
+        n = 7
+    case [_, arg1, arg2]:
+        m = int(arg1)
+        n = int(arg2)
+
+pprint.pprint(make_grid(m, n), width=40)
