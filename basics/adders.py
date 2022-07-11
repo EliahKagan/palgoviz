9a3defb1df13c2274838f0760962a7b3f348d3c4
Eliah Kagan <degeneracypressure@gmail.com>
2022-07-11 19:24:19 -0400

    Describe and implement adders.Adder exercise

--- basics/adders.py
+++ basics/adders.py
@@ -1,4 +1,4 @@
-"""Making functions that add to the same value."""
+"""Callables that add a fixed value to their argument."""
 
 
 def make_adder(left_addend):
@@ -19,6 +19,68 @@ def make_adder(left_addend):
     def adder(right_addend):
         return left_addend + right_addend
     return adder
+
+
+class Adder:
+    """
+    Callable object that adds its argument to the addend given on construction.
+
+    The fixed added an Adder stores and uses is a left addend, which matters in
+    some noncommutative meanings of "+", such as sequence concatenation. This
+    is the class version of make_adder, with some more features classes allow.
+
+    >>> a = Adder(7)
+    >>> a(4)
+    11
+    >>> a(10)
+    17
+    >>> Adder(6)(2)
+    8
+    >>> Adder('cat')
+    Adder('cat')
+    >>> _(' dog')
+    'cat dog'
+    >>> {Adder(7), Adder(7), Adder(6), Adder(7.0)} == {Adder(6), Adder(7)}
+    True
+    >>> a.left_addend
+    7
+    >>> a.right_addend
+    Traceback (most recent call last):
+      ...
+    AttributeError: 'Adder' object has no attribute 'right_addend'
+    >>> a.left_addend = 8
+    Traceback (most recent call last):
+      ...
+    AttributeError: can't set attribute 'left_addend'
+    """
+
+    __slots__ = ('_left_addend',)
+
+    def __init__(self, left_addend):
+        """Create an adder for the given addend."""
+        self._left_addend = left_addend
+
+    def __repr__(self):
+        """Representation of this Adder as Python code."""
+        return f'{type(self).__name__}({self.left_addend!r})'
+
+    def __call__(self, right_addend):
+        """Add the stored left addend to the passed right addend."""
+        return self.left_addend + right_addend
+
+    def __eq__(self, other):
+        """Two adders are equal if their left addends are equal."""
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.left_addend == other.left_addend
+
+    def __hash__(self):
+        return hash(self.left_addend)
+
+    @property
+    def left_addend(self):
+        """The left addend this adder adds to its argument when called."""
+        return self._left_addend
 
 
 if __name__ == '__main__':
