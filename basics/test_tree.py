d4c7a538b11ed2f49cbd8dc372d8002e746eaee8
Eliah Kagan <degeneracypressure@gmail.com>
2022-07-19 00:51:30 -0400

    Write most tests for Node and FrozenNode
    These tests are just for the node classes, not yet for the
    operations on them that are provided by top-level functions
    alongside them in the tree module.

--- basics/test_tree.py
+++ basics/test_tree.py
@@ -1,138 +1,212 @@
 """Tests for tree.py."""
 
+from abc import ABC, abstractmethod
+import inspect
 import unittest
 
-from tree import Node
+import tree
 
 
-@unittest.skip(  # FIXME: Make and implementation a decision and remove @skip.
-    'Unsure if these construction requirements make a useful exercise.')
-class TestNode(unittest.TestCase):
-    """
-    Tests for the Node class.
+class _TestNodeBase(ABC, unittest.TestCase):
+    """Base class providing shared tests for node classes."""
 
-    In this class, an internal node with no right child is a "lefter" and an
-    internal node with no left child is a "righter." This terminology is not at
-    all standard and may not often be justified, but here I think it makes the
-    test names much more readable. Likewise a "full" node is one with two
-    children, which is slightly less strange since a full binary tree is one
-    whose internal nodes are all "full" in this sense, but still not standard.
-    """
+    @property
+    @abstractmethod
+    def node_type(self):
+        """The node class being tested."""
+        raise NotImplementedError
 
-    def test_can_construct_leaf_without_child_args(self):
-        leaf = Node(10)
-        with self.subTest(attribute='element'):
-            self.assertEqual(leaf.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIsNone(leaf.left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(leaf.right)
-
-    def test_can_construct_leaf_with_both_child_positional_args(self):
-        leaf = Node(10, None, None)
-        with self.subTest(attribute='element'):
-            self.assertEqual(leaf.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIsNone(leaf.left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(leaf.right)
-
-    def test_can_construct_leaf_with_child_keyword_args_left_right(self):
-        leaf = Node(10, left=None, right=None)
-        with self.subTest(attribute='element'):
-            self.assertEqual(leaf.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIsNone(leaf.left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(leaf.right)
-
-    def test_can_construct_leaf_with_child_keyword_args_right_left(self):
-        leaf = Node(10, right=None, left=None)
-        with self.subTest(attribute='element'):
-            self.assertEqual(leaf.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIsNone(leaf.left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(leaf.right)
-
-    def test_can_construct_leaf_with_left_child_keyword_arg(self):
-        leaf = Node(10, left=None)
-        with self.subTest(attribute='element'):
-            self.assertEqual(leaf.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIsNone(leaf.left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(leaf.right)
-
-    def test_can_construct_leaf_with_right_child_keyword_arg(self):
-        leaf = Node(10, right=None)
-        with self.subTest(attribute='element'):
-            self.assertEqual(leaf.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIsNone(leaf.left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(leaf.right)
-
-    def test_cannot_construct_leaf_with_single_child_positional_arg(self):
+    def test_cannot_construct_with_no_args(self):
         with self.assertRaises(TypeError):
-            Node(10, None)
+            self.node_type()
 
-    def test_cannot_construct_leaf_with_mixed_child_args(self):
-        """Children, if given, must both be positional or both be keywords."""
-        with self.subTest(keyword_child='left'):
-            with self.assertRaises(TypeError):
-                Node(10, None, left=None)
+    def test_one_positional_arg_is_element(self):
+        node = self.node_type('hello')
+        self.assertEqual(node.element, 'hello')
 
-        with self.subTest(keyword_child='right'):
-            with self.assertRaises(TypeError):
-                Node(10, None, right=None)
+    def test_one_arg_constructs_with_no_left_child(self):
+        node = self.node_type('hello')
+        self.assertIsNone(node.left)
 
-    def test_can_construct_lefter_with_both_child_positional_args(self):
-        left = Node(20)
-        root = Node(10, left, None)
-        with self.subTest(attribute='element'):
-            self.assertEqual(root.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIs(root.left, left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(root.right)
+    def test_one_arg_constructs_with_no_right_child(self):
+        node = self.node_type('hello')
+        self.assertIsNone(node.right)
 
-    def test_can_construct_lefter_with_left_child_keyword_arg(self):
-        left = Node(20)
-        root = Node(10, left=left)
-        with self.subTest(attribute='element'):
-            self.assertEqual(root.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIs(root.left, left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(root.right)
+    def test_three_positional_args_first_is_element(self):
+        left = self.node_type('LC')
+        right = self.node_type('RC')
+        root = self.node_type('P', left, right)
+        self.assertEqual(root.element, 'P')
 
-    def test_can_construct_lefter_with_child_keyword_args_left_right(self):
-        left = Node(20)
-        root = Node(10, left=left, right=None)
-        with self.subTest(attribute='element'):
-            self.assertEqual(root.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIs(root.left, left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(root.right)
+    def test_three_positional_args_second_is_left_child(self):
+        left = self.node_type('LC')
+        right = self.node_type('RC')
+        root = self.node_type('P', left, right)
+        self.assertIs(root.left, left)
 
-    def test_can_construct_lefter_with_child_keyword_args_right_left(self):
-        left = Node(20)
-        root = Node(10, right=None, left=left)
-        with self.subTest(attribute='element'):
-            self.assertEqual(root.element, 10)
-        with self.subTest(attribute='left'):
-            self.assertIs(root.left, left)
-        with self.subTest(attribute='right'):
-            self.assertIsNone(root.right)
+    def test_three_positional_args_third_is_right_child(self):
+        left = self.node_type('LC')
+        right = self.node_type('RC')
+        root = self.node_type('P', left, right)
+        self.assertIs(root.right, right)
 
-    # FIXME: Write the rest of these construction tests.
+    def test_positional_arg_is_element_with_left_keyword_arg(self):
+        left = self.node_type('LC')
+        root = self.node_type('P', left=left)
+        self.assertEqual(root.element, 'P')
 
-    def test_repr_of_leaf_is_code_omitting_children(self):
-        leaf = Node(10)
+    def test_with_positional_arg_left_keyword_arg_is_left_child(self):
+        left = self.node_type('LC')
+        root = self.node_type('P', left=left)
+        self.assertIs(root.left, left)
 
-    def test_repr_of_internal_node_is_code_including_children(self):
-        root = Node(10, Node(20), Node(30))
+    def test_no_right_child_with_positional_and_left_keyword_arg(self):
+        left = self.node_type('LC')
+        root = self.node_type('P', left=left)
+        self.assertIsNone(root.right)
 
-    # FIXME: Write the rest of the tests that are not about construction.
+    def test_positional_arg_is_element_with_right_keyword_arg(self):
+        right = self.node_type('RC')
+        root = self.node_type('P', right=right)
+        self.assertEqual(root.element, 'P')
+
+    def test_with_positional_arg_right_keyword_arg_is_right_child(self):
+        right = self.node_type('RC')
+        root = self.node_type('P', right=right)
+        self.assertIs(root.right, right)
+
+    def test_no_left_child_with_positional_and_right_keyword_arg(self):
+        right = self.node_type('RC')
+        root = self.node_type('P', right=right)
+        self.assertIsNone(root.left)
+
+    def test_with_positional_and_both_keyword_args_positional_is_element(self):
+        left = self.node_type('LC')
+        right = self.node_type('RC')
+        root = self.node_type('P', left=left, right=right)
+        self.assertEqual(root.element, 'P')
+
+    def test_with_positional_and_both_keyword_args_left_is_left_child(self):
+        left = self.node_type('LC')
+        right = self.node_type('RC')
+        root = self.node_type('P', left=left, right=right)
+        self.assertIs(root.left, left)
+
+    def test_with_positional_and_both_keyword_args_right_is_right_child(self):
+        left = self.node_type('LC')
+        right = self.node_type('RC')
+        root = self.node_type('P', left=left, right=right)
+        self.assertIs(root.right, right)
+
+    def test_cannot_construct_without_element_arg(self):
+        left = self.node_type('LC')
+        right = self.node_type('RC')
+        with self.assertRaises(TypeError):
+            self.node_type(left=left, right=right)
+
+    def test_cannot_reassign_element(self):
+        node = self.node_type('hello')
+        with self.assertRaises(AttributeError):
+            node.element = 'goodbye'
+
+    def test_cannot_create_attributes_by_assignment(self):
+        node = self.node_type('hello')
+        with self.assertRaises(AttributeError):
+            node.center = 'CC'
+
+    def test_no_instance_dictionary(self):
+        """To save memory, nodes shouldn't usually have __dict__."""
+        node = self.node_type('hello')
+        with self.assertRaises(AttributeError):
+            node.__dict__
+
+    def test_element_documented_as_element(self):
+        doc = inspect.getdoc(self.node_type.element)
+        self.assertEqual(doc, 'The element held in this node.')
+
+    def test_left_documented_as_left_child(self):
+        doc = inspect.getdoc(self.node_type.left)
+        self.assertEqual(doc, 'The left child.')
+
+    def test_right_documented_as_right_child(self):
+        doc = inspect.getdoc(self.node_type.right)
+        self.assertEqual(doc, 'The right child.')
+
+
+class TestNode(_TestNodeBase):
+    """Tests for the Node class."""
+
+    @property
+    def node_type(self):
+        return tree.Node
+
+    def test_can_reassign_left_child(self):
+        root = tree.Node('P')
+        left = tree.Node('LC')
+        root.left = left
+        self.assertIs(root.left, left)
+
+    def test_can_reassign_left_child_none(self):
+        left = tree.Node('LC')
+        root = tree.Node('P', left=left)
+        root.left = None
+        self.assertIsNone(root.left)
+
+    def test_can_reassign_right_child(self):
+        root = tree.Node('P')
+        right = tree.Node('RC')
+        root.right = right
+        self.assertIs(root.right, right)
+
+    def test_can_reassign_right_child_none(self):
+        right = tree.Node('RC')
+        root = tree.Node('P', right=right)
+        root.right = None
+        self.assertIsNone(root.right)
+
+    def test_left_is_not_property(self):
+        """It can be reassigned and isn't validated. Not worth a property."""
+        self.assertNotIsInstance(tree.Node.left, property)
+
+    def test_right_is_not_property(self):
+        """It can be reassigned and isn't validated. Not worth a property."""
+        self.assertNotIsInstance(tree.Node.right, property)
+
+    # FIXME: Add repr and equality comparison tests.
+
+
+class TestFrozenNode(_TestNodeBase):
+    """Tests for the FrozenNode class."""
+
+    @property
+    def node_type(self):
+        return tree.FrozenNode
+
+    def test_cannot_reassign_left_child(self):
+        root = tree.FrozenNode('P')
+        left = tree.FrozenNode('LC')
+        with self.assertRaises(AttributeError):
+            root.left = left
+
+    def test_cannot_reassign_left_child_none(self):
+        left = tree.FrozenNode('LC')
+        root = tree.FrozenNode('P', left=left)
+        with self.assertRaises(AttributeError):
+            root.left = None
+
+    def test_cannot_reassign_right_child(self):
+        root = tree.FrozenNode('P')
+        right = tree.FrozenNode('RC')
+        with self.assertRaises(AttributeError):
+            root.right = right
+
+    def test_cannot_reassign_right_child_none(self):
+        right = tree.FrozenNode('RC')
+        root = tree.FrozenNode('P', right=right)
+        with self.assertRaises(AttributeError):
+            root.right = None
+
+    # FIXME: Add repr and equality comparison tests.
+
+
+del _TestNodeBase
