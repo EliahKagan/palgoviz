63709e689afe535f08f99e4e828f645228a02f3f
David Vassallo <vassallo.davidm@gmail.com>
2022-09-25 13:37:07 -0400

    Pass runs and spin count to main

--- basics/datarace.py
+++ basics/datarace.py
@@ -2,25 +2,23 @@
 
 """Attempting data races on thread-safe and thread-unsafe singletons."""
 
-__all__ = ['SPIN_COUNT', 'make_singleton', 'one_run', 'main']
+__all__ = ['make_singleton', 'one_run', 'run_multiple', 'main']
 
 import threading
 
-# Eventually we may adjust this to get ~10% collision rate in the unsafe demo.
-SPIN_COUNT = 1_300_000
-
 
 # TODO: Have the thread-safe Singleton implementation do it via locking.
-def make_singleton(*, safe):
+def make_singleton(*, safe, spin_count):
+    """Make a singleton class, optionally thread safe."""
     class Singleton:
-        """Lazy implementation of the singleton pattern. Not thread-safe."""
+        """Lazy implementation of the singleton pattern."""
 
         _instance = None
 
         def __new__(cls):
             if cls._instance is None:
                 # Simulate nontrivial work to construct the instance.
-                for _ in range(SPIN_COUNT):
+                for _ in range(spin_count):
                     pass
 
                 cls._instance = super().__new__(cls)
@@ -33,9 +31,9 @@ def make_singleton(*, safe):
     return Singleton
 
 
-def one_run(*, safe):
-
-    Singleton = make_singleton(safe=safe)
+def one_run(*, safe, spin_count):
+    """Run the data race test once."""
+    Singleton = make_singleton(safe=safe, spin_count=spin_count)
 
     a = None
     b = None
@@ -58,22 +56,20 @@ def one_run(*, safe):
     assert a is not None
     assert b is not None
 
-    return 1 if a is b else 0
+    return a is b
 
 
-def run_multiple(*, runs, safe):
-    x = 0
-    for _ in range(runs):
-        x += one_run(safe=safe)
+def run_multiple(*, runs, safe, spin_count):
+    """Run the data race test several times."""
+    successes = sum(one_run(safe=safe, spin_count=spin_count)
+                    for _ in range(runs))
+    print(f'{successes} successful runs out of {runs}. {safe = }')
 
-    print(f'{x} successful runs out of {runs}. {safe = }')
 
-
-def main():
-    """Run the test."""
-
-    run_multiple(runs=100, safe=True)
-    run_multiple(runs=100, safe=False)
+def main(*, runs=100, spin_count=1_300_000):
+    """Run the test for both thread safe and thread unsafe."""
+    run_multiple(runs=runs, safe=True, spin_count=spin_count)
+    run_multiple(runs=runs, safe=False, spin_count=spin_count)
 
 
 if __name__ == '__main__':
