a4d806df39f92a8418c87cd3abb22147a41bf07f
Eliah Kagan <degeneracypressure@gmail.com>
2022-10-06 15:24:18 -0400

    Add typed singly linked list exercises

--- basics/aggregate.py
+++ basics/aggregate.py
@@ -26,8 +26,11 @@ across functions, and across classes, when sharing it would obscure anything or
 make any alternative less self contained -- except where noted otherwise.
 """
 
+from __future__ import annotations
+
 import collections
-from collections.abc import Iterable
+from collections.abc import Iterable, Iterator
+import dataclasses
 import math
 import types
 import typing
@@ -1700,6 +1703,191 @@ def summarize_as_dict_alt(values, *, precision=DEFAULT_PRECISION):
     return attrs.asdict(summary)
 
 
+class StrNode:
+    """
+    A node in a mutable singly linked list of strings, manually implemented.
+
+    Both attributes are mutable. Both attributes, and all methods, have type
+    annotations. Element types are not validated at runtime, so this could be
+    used to store any kind of values, but static type checkers will report
+    non-string values as errors. It's often better to make a generic Node type
+    and use Node[str] instead of StrNode, but that is not done here. Also, the
+    repr always shows both arguments, even if next=None, and it shows them as
+    keyword arguments. This is all to make StrNode work like StrNodeA below,
+    while still keeping the implementation of StrNodeA very straightforward.
+
+    >>> StrNode('foo', StrNode('bar'))
+    StrNode(value='foo', next=StrNode(value='bar', next=None))
+    >>> StrNode.build('foo', 'bar')
+    StrNode(value='foo', next=StrNode(value='bar', next=None))
+    >>> _.value, _.next, _.next.value, _.next.next
+    ('foo', StrNode(value='bar', next=None), 'bar', None)
+
+    >>> head = StrNode('X')
+    >>> head == StrNode('X')  # Not doing structural equality comparison.
+    False
+    >>> vars(head)  # No instance dictionary.
+    Traceback (most recent call last):
+      ...
+    TypeError: vars() argument must have __dict__ attribute
+    >>> import weakref; weakref.ref(head)  # No weak reference support.
+    Traceback (most recent call last):
+      ...
+    TypeError: cannot create weak reference to 'StrNode' object
+
+    >>> head.value = 'W'
+    >>> head.next = StrNode.build('Y', 'Z')
+    >>> head  # doctest: +NORMALIZE_WHITESPACE
+    StrNode(value='W',
+            next=StrNode(value='Y', next=StrNode(value='Z', next=None)))
+    """
+
+    __slots__ = ('value', 'next')
+
+    value: str
+    next: StrNode | None
+
+    @classmethod
+    def build(cls, *values: str) -> StrNode | None:
+        """Make a singly linked list of the given values. Return the head."""
+        acc = None
+        for value in reversed(values):
+            acc = cls(value, acc)
+        return acc
+
+    def __init__(self, value: str, next: StrNode | None = None) -> None:
+        """Create a node with the given element value, and next node if any."""
+        self.value = value
+        self.next = next
+
+    def __repr__(self) -> str:
+        """Python code repr for debugging, using keyword arguments."""
+        return (type(self).__name__
+                + f'(value={self.value!r}, next={self.next!r})')
+
+
+@attrs.mutable(eq=False, weakref_slot=False)
+class StrNodeA:
+    """
+    A node in a mutable singly linked list of strings, as an attrs data class.
+
+    This class uses the attrs library's modern API. It has type annotations.
+
+    >>> StrNodeA('foo', StrNodeA('bar'))
+    StrNodeA(value='foo', next=StrNodeA(value='bar', next=None))
+    >>> StrNodeA.build('foo', 'bar')
+    StrNodeA(value='foo', next=StrNodeA(value='bar', next=None))
+    >>> _.value, _.next, _.next.value, _.next.next
+    ('foo', StrNodeA(value='bar', next=None), 'bar', None)
+
+    >>> head = StrNodeA('X')
+    >>> head == StrNodeA('X')  # Not doing structural equality comparison.
+    False
+    >>> vars(head)  # No instance dictionary.
+    Traceback (most recent call last):
+      ...
+    TypeError: vars() argument must have __dict__ attribute
+    >>> import weakref; weakref.ref(head)  # No weak reference support.
+    Traceback (most recent call last):
+      ...
+    TypeError: cannot create weak reference to 'StrNodeA' object
+
+    >>> head.value = 'W'
+    >>> head.next = StrNodeA.build('Y', 'Z')
+    >>> head  # doctest: +NORMALIZE_WHITESPACE
+    StrNodeA(value='W',
+             next=StrNodeA(value='Y', next=StrNodeA(value='Z', next=None)))
+    """
+
+    value: str
+    next: StrNodeA | None = None
+
+    @classmethod
+    def build(cls, *values: str) -> StrNodeA | None:
+        """Make a singly linked list of the given values. Return the head."""
+        acc = None
+        for value in reversed(values):
+            acc = cls(value, acc)
+        return acc
+
+
+@dataclasses.dataclass(slots=True, eq=False)
+class StrNodeD:
+    """
+    A node in a mutable singly linked list of strings, as a @dataclass.
+
+    This uses @dataclasses.dataclass, and therefore has type annotations.
+
+    >>> StrNodeD('foo', StrNodeD('bar'))
+    StrNodeD(value='foo', next=StrNodeD(value='bar', next=None))
+    >>> StrNodeD.build('foo', 'bar')
+    StrNodeD(value='foo', next=StrNodeD(value='bar', next=None))
+    >>> _.value, _.next, _.next.value, _.next.next
+    ('foo', StrNodeD(value='bar', next=None), 'bar', None)
+
+    >>> head = StrNodeD('X')
+    >>> head == StrNodeD('X')  # Not doing structural equality comparison.
+    False
+    >>> vars(head)  # No instance dictionary.
+    Traceback (most recent call last):
+      ...
+    TypeError: vars() argument must have __dict__ attribute
+    >>> import weakref; weakref.ref(head)  # No weak reference support.
+    Traceback (most recent call last):
+      ...
+    TypeError: cannot create weak reference to 'StrNodeD' object
+
+    >>> head.value = 'W'
+    >>> head.next = StrNodeD.build('Y', 'Z')
+    >>> head  # doctest: +NORMALIZE_WHITESPACE
+    StrNodeD(value='W',
+             next=StrNodeD(value='Y', next=StrNodeD(value='Z', next=None)))
+    """
+
+    value: str
+    next: StrNodeD | None = None
+
+    @classmethod
+    def build(cls, *values: str) -> StrNodeD | None:
+        """Make a singly linked list of the given values. Return the head."""
+        acc = None
+        for value in reversed(values):
+            acc = cls(value, acc)
+        return acc
+
+
+def traverse(node: StrNode | StrNodeA | StrNodeD | None) -> Iterator[str]:
+    """
+    Yield all values in a singly linked list, from front to back.
+
+    >>> import string
+
+    >>> list(traverse(StrNode('foo', StrNode('bar'))))
+    ['foo', 'bar']
+    >>> list(traverse(StrNode.build('foo', 'bar')))
+    ['foo', 'bar']
+    >>> ''.join(traverse(StrNode.build(string.ascii_lowercase)))
+    'abcdefghijklmnopqrstuvwxyz'
+
+    >>> list(traverse(StrNodeA('foo', StrNodeA('bar'))))
+    ['foo', 'bar']
+    >>> list(traverse(StrNodeA.build('foo', 'bar')))
+    ['foo', 'bar']
+    >>> ''.join(traverse(StrNodeA.build(string.ascii_lowercase)))
+    'abcdefghijklmnopqrstuvwxyz'
+
+    >>> list(traverse(StrNodeD('foo', StrNodeD('bar'))))
+    ['foo', 'bar']
+    >>> list(traverse(StrNodeD.build('foo', 'bar')))
+    ['foo', 'bar']
+    >>> ''.join(traverse(StrNodeD.build(string.ascii_lowercase)))
+    'abcdefghijklmnopqrstuvwxyz'
+    """
+    while node:
+        yield node.value
+        node = node.next
+
+
 __all__ = [thing.__name__ for thing in (  # type: ignore[attr-defined]
     summarize_as_tuple,
     summarize_as_dict,
@@ -1729,6 +1917,10 @@ __all__ = [thing.__name__ for thing in (  # type: ignore[attr-defined]
     summarize_as_typed_mutable_attrs,
     summarize_as_tuple_alt,
     summarize_as_dict_alt,
+
+    StrNode,
+    StrNodeA,
+    traverse,
 )]
 
 
