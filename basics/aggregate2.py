69c828d973ae6ac041bae85ff4395644c4ac6865
Eliah Kagan <degeneracypressure@gmail.com>
2022-10-07 04:26:16 -0400

    Mostly implement Point & Vector in aggregate2

--- basics/aggregate2.py
+++ basics/aggregate2.py
@@ -17,6 +17,7 @@ import collections
 from collections.abc import Iterator
 import dataclasses
 import math
+import numbers
 import typing
 
 import attrs
@@ -29,18 +30,19 @@ Coords.__doc__ = """
 
     This is made using the type factory function in the collections module. Its
     intended use is as the return type for functions that will often have their
-    results unpacked into multiple variable by an assignment at the call site,
+    results unpacked into multiple variables by an assignment at the call site,
     but may sometimes be retained in original form. Then it supports having its
     distance from the origin taken, by calling the abs builtin on it. (Even for
     objects that are not numbers, customizing abs is a common way to provide
-    magnitude, so long as the magnitude is a number. Whether a Coords instance
-    is being used to represent something with a magnitude varies by use case.)
+    magnitude, so long as the magnitude is guaranteed to be a nonnegative real
+    number. Whether a Coords instance is being used to represent something with
+    a magnitude varies by use case.)
 
     This implementation does not use inheritance, besides the inheritance from
     tuple that is taken care of by the type factory function. [FIXME: So no
-    class statement will actually be involved here. This is just a way to give
-    the docstring, which should be preserved and given to the type created by
-    the type factory, and the class statement otherwise removed form the code.]
+    class statement will actually be involved here. This is just a place to put
+    the docstring before the type is implemented. Give this docstring to the
+    type created by the type factory and remove this class statement entirely.]
 
     >>> p = Coords(4, -12, 3)
     >>> p
@@ -92,7 +94,7 @@ class TypedCoords(typing.NamedTuple):
     Named tuple of Cartesian coordinates in 3-space. Has type annotations.
 
     This is like Coords/CoordsAlt but "typed." It is created with the facility
-    in the typing module.
+    in the typing module for making named tuple types with typed attributes.
 
     >>> p = TypedCoords(4, -12, 3)
     >>> p
@@ -112,6 +114,7 @@ class TypedCoords(typing.NamedTuple):
         return math.hypot(*self)
 
 
+@attrs.frozen
 class Point:
     """
     A point in 3-space, represented by its x-, y-, and z-coordinates.
@@ -125,18 +128,173 @@ class Point:
     of two Point objects is a Vector object representing the displacement from
     the subtrahend to the minuend (so p - q points from q to p).
 
-    FIXME: Needs tests.
+    Unlike Vector, where the object with all zero coordinates is especially
+    important, which point we choose as the origin is entirely a matter of
+    convention. So it is somewhat inelegant that Point allows omitting any
+    combination of coordinate arguments on construction, with the same effect
+    as passing 0. But it does, because this makes it much easier to use a Point
+    to solve problems in a 1- or 2-dimensional coordinate system, by setting
+    the other two or one coordinates, respectively, to zero, and ignoring them.
+
+    >>> Point(1, 2, 3), Point(1, 2), Point(1)
+    (Point(x=1, y=2, z=3), Point(x=1, y=2, z=0), Point(x=1, y=0, z=0))
+    >>> Point(z=3), Point(1, z=3), Point(y=2, x=1)
+    (Point(x=0, y=0, z=3), Point(x=1, y=0, z=3), Point(x=1, y=2, z=0))
+    >>> {Point(), Point(0), Point(0, 0), Point(0, 0, 0)}
+    {Point(x=0, y=0, z=0)}
+
+    >>> p = Point(3.1, -7, 5.0); q = Point(3.1, 1.6, 5)
+    >>> p == Point(3.1, -7.0, 5), p == q, attrs.evolve(p, y=1.6) == q, p == q
+    (True, False, True, False)
+    >>> p.y = 1.6
+    Traceback (most recent call last):
+      ...
+    attr.exceptions.FrozenInstanceError
+    >>> p.t = -4  # No t attribute. This is not Minkowski space.
+    Traceback (most recent call last):
+      ...
+    attr.exceptions.FrozenInstanceError
+
+    >>> hasattr(p, '__dict__'), attrs.asdict(p)
+    (False, {'x': 3.1, 'y': -7, 'z': 5.0})
+    >>> abs(p)
+    Traceback (most recent call last):
+      ...
+    TypeError: bad operand type for abs(): 'Point'
+    >>> p.coords, p.coords == attrs.astuple(p), round(abs(p.coords), 5)
+    (Coords(x=3.1, y=-7, z=5.0), True, 9.14385)
+
+    >>> p - q, q - p
+    (Vector(x=0.0, y=-8.6, z=0.0), Vector(x=0.0, y=8.6, z=0.0))
+    >>> p + Vector(y=1.2), Vector(y=1.2) + p
+    (Point(x=3.1, y=-5.8, z=5.0), Point(x=3.1, y=-5.8, z=5.0))
+    >>> p - Vector(y=1.2)
+    Point(x=3.1, y=-8.2, z=5.0)
+    >>> Vector(y=1.2) - p  # We can't subtract a point from a vector.
+    Traceback (most recent call last):
+      ...
+    TypeError: unsupported operand type(s) for -: 'Vector' and 'Point'
+    >>> p + 3.14159  # It makes no sense to add a point and a scalar.
+    Traceback (most recent call last):
+      ...
+    TypeError: unsupported operand type(s) for +: 'Point' and 'float'
     """
-    # FIXME: Needs implementation.
+
+    x = attrs.field(default=0)
+    y = attrs.field(default=0)
+    z = attrs.field(default=0)
+
+    def __add__(self, addend):
+        match addend:
+            case Vector(x, y, z):
+                return Point(self.x + x, self.y + y, self.z + z)
+            case _:
+                return NotImplemented
+
+    __radd__ = __add__
+
+    def __sub__(self, subtrahend):
+        match subtrahend:
+            case Point(x, y, z):
+                return Vector(self.x - x, self.y - y, self.z - z)
+            case Vector(x, y, z):
+                return Point(self.x - x, self.y - y, self.z - z)
+            case _:
+                return NotImplemented
+
+    def __rsub__(self, minuend):
+        match minuend:
+            case Point(x, y, z):
+                return Vector(x - self.x, y - self.y, z - self.z)
+            case _:
+                return NotImplemented
+
+    @property
+    def coords(self):
+        return Coords(self.x, self.y, self.z)
 
 
+@attrs.frozen
 class Vector:
     """
     A vector in 3-space, represented by its x-, y-, and z- components.
 
-    FIXME: Needs description and tests.
+    Like Point, this is implemented as an attrs data class, with no type
+    annotations, using the modern attrs API. Geometric vectors represent
+    translations, also called displacements. Under this conceptualization, the
+    meaning of a Vector object fundamentally relates to its ability to be added
+    to a Point object to get another Point object. Vectors can also be added
+    and subtracted from each other, multiplied by real-valued scalars, and
+    divided by nonzero real-valued scalars, all of which return a Vector.
+
+    There are two conceptually interesting things about Vector. First, notice
+    that, while a Vector takes its meaning from the relationship it expresses
+    between Points, there is no way to get a Point if you don't have one to
+    start with (or the ability to explicitly call the Point type to make one).
+    With just Points, we can get a Vector; with just Vectors, even scalars too,
+    we cannot get a Point. Second, notice that the identity (a b) v = a (b v),
+    where a and b are scalars and v is a vector, gives meaning to the operation
+    of multiplying two scalars: composition of the dilation transformations
+    they represent. So too, the identity p + (u + v) = (p + u) + v, where u and
+    v are vectors and p is a point, gives meaning to the operation of adding
+    two vectors: composition of the translation transformations they represent.
+
+    Many aspects of choice of coordinate system are arbitrary, but there is
+    nothing arbitrary about the zero Vector: it is the Vector that, when added
+    to any Point, gives that same Point. More broadly, assuming prior choice of
+    axes, it makes sense that if we don't want to displace parallel to some
+    axis, we would omit the component along that axis when specifying a Vector
+    that does so. So, like Point, all arguments are optional when constructing
+    a Vector; unlike Point, however, it is elegant that Vector allows this.
+
+    >>> 0.5 * Vector(1, 2, 3) * 0.5
+    Vector(x=0.25, y=0.5, z=0.75)
+
+    FIXME: Needs more doctests.
     """
-    # FIXME: Needs implementation.
+
+    x = attrs.field(default=0)
+    y = attrs.field(default=0)
+    z = attrs.field(default=0)
+
+    def __add__(self, vector):
+        match vector:
+            case Vector(x, y, z):
+                return Vector(self.x + x, self.y + y, self.z + z)
+            case _:
+                return NotImplemented
+
+    __radd__ = __add__
+
+    def __sub__(self, vector):
+        match vector:
+            case Vector(x, y, z):
+                return Vector(self.x - x, self.y - y, self.z - z)
+            case _:
+                return NotImplemented
+
+    def __rsub__(self, vector):
+        match vector:
+            case Vector(x, y, z):
+                return Vector(x - self.x, y - self.y, z - self.z)
+            case _:
+                return NotImplemented
+
+    def __mul__(self, scalar):
+        if isinstance(scalar, numbers.Real):
+            return Vector(self.x * scalar, self.y * scalar, self.z * scalar)
+        return NotImplemented
+
+    __rmul__ = __mul__
+
+    def __truediv__(self, scalar):
+        if isinstance(scalar, numbers.Real):
+            return Vector(self.x / scalar, self.y / scalar, self.z / scalar)
+        return NotImplemented
+
+    @property
+    def coords(self):
+        return Coords(self.x, self.y, self.z)
 
 
 class StrNode:
