6108a69670c079832509c8fc96f2505c20c96c87
Eliah Kagan <degeneracypressure@gmail.com>
2022-10-23 20:08:04 -0400

    Make restructure script more user-friendly
    And add more comments to explain what it is doing and why.

--- meta/shared.sh
+++ meta/shared.sh
@@ -1,7 +1,5 @@
-# Shared code for the restructure and patch-imports scripts.
-#
-# Both scripts use the functions. Both also need the directory check: usually
-# patch-imports will be run by restructure, but it may also be run on is own.
+# Shared code for the restructure and patch-imports scripts. Source this file
+# to define helpers, check we are in the right place, and validate arguments.
 
 msg() {
     printf '%s: %s\n' "$0" "$1" >&2
@@ -16,5 +14,21 @@ die() {
     exit 1
 }
 
+# If passing "-b" to sed may not be supported, fail before making any changes.
+sed --version | grep -qF '(GNU sed)' || die 'need sed to be GNU sed'
+
+# This version doesn't support command-line args, so make sure there are none.
+[ "$#" -eq 0 ] || die 'too many arguments'
+
 # We should be in the top-level repository directory.
 [ -d .git ] || die 'not in repository root'
+
+# And this directory should be called algoviz.
+[ "$(basename -- "$PWD")" = algoviz ] ||
+    die 'current directory is not called algoviz'
+
+# And its parent directory should NOT be called algoviz. If the user is
+# accidentally in algoviz/algoviz, then there should be no .git directory, but
+# this is simple and easy and safeguards against mistakes involving re-cloning.
+[ "$(basename -- "$(dirname -- "$PWD")")" != algoviz ] ||
+    die 'current directory is algoviz/algoviz but we want the parent'
