f2b698a8001d0b88f82df49365988e8cc30c6a07
Eliah Kagan <degeneracypressure@gmail.com>
2023-02-12 23:17:51 +0000

    Carry in config when repo is *directly* in Windows
    This specific situation was not covered before in this repository.
    
    This now covers the hopefully infrequent case where the user
    reopens the local directory, in a Windows system, in the container,
    rather than cloning the repository in the container.
    
    In that situation, initializeCommand runs *directly* on the host
    Windows system. In contrast, in other situations on Windows, it
    does not run *directly* on Windows, but instead in the Linux-based
    environment that Docker uses do its work. (This is currently either
    a WSL 2 system or a Linux-based system running directly in a
    Hyper-V virtual machine, depending which backend Docker is
    configured to use.)
    
    See:
    
    - https://github.com/EliahKagan/portable-initialize
    - https://github.com/EliahKagan/batch/blob/main/initialize.cmd
    - https://github.com/EliahKagan/VidDraw/blob/master/.devcontainer/initialize.cmd

---
+++ .devcontainer/initialize.cmd
@@ -0,0 +1,57 @@
+:: Copyright (c) 2023 Eliah Kagan
+::
+:: Permission to use, copy, modify, and/or distribute this software for any
+:: purpose with or without fee is hereby granted.
+::
+:: THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+:: WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+:: MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+:: SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+:: WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+:: OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+:: CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+@echo off
+setlocal enableextensions
+
+:: Use the code page for UTF-8.
+chcp 65001 >NUL
+
+set "conf_path=.inherited-configuration"
+set "script_name=%~nx0"
+
+goto begin
+
+:msg
+    echo/%script_name%: %* >&2
+    exit /b
+
+:push_in
+    setlocal
+    set "name=%1"
+
+    git config -- "%name%" >NUL
+
+    if %ERRORLEVEL% NEQ 0 (
+        call :msg skipping: %name%
+        exit /b 0
+    )
+
+    for /f "usebackq delims=" %%G in (`git config -- "%name%"`) do (
+        set "value=%%G"
+    )
+
+    call :msg carrying in: %name%=%value%
+    echo/%name% %value% >>"%conf_path%"
+    exit /b
+
+:begin
+    del "%conf_path%" 2>NUL
+
+    :: If the host clones the repo Windows-style, have the container play
+    :: along.
+    call :push_in core.autocrlf
+
+    :: Usually these are automatically set in the container, but not always.
+    call :push_in user.name
+    call :push_in user.email
